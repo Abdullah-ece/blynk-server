180157fbd291a8aee0df26377398ac5c77c94a2e
Igor Gaidaichuk <i.gaidai4uk@gmail.com>
2017-03-30 01:02:09 +0200

    Make login/create acc page according to mockups #11 - validation

--- server/http-dashboard/src/main/webapp/js/app.js
+++ server/http-dashboard/src/main/webapp/js/app.js
@@ -6,31 +6,49 @@ import {Form, Icon, Input, Button, Checkbox} from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 const FormItem = Form.Item;
 
+function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
 class NormalLoginForm extends React.Component {
+    componentDidMount() {
+        this.props.form.validateFields();
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+
             }
         });
     }
 
     render() {
-        const {getFieldDecorator} = this.props.form;
+        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+        const userNameError = isFieldTouched('userName') && getFieldError('userName');
+        const passwordError = isFieldTouched('password') && getFieldError('password');
+
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form">
+            <Form onSubmit={this.handleSubmit.bind(this)} className="login-form">
                 <FormItem>
                     <span className="form-header">Log in to Blynk</span>
                 </FormItem>
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{required: true, message: 'Please input your username!'}],
-                    })(
+                <FormItem validateStatus={userNameError ? 'error' : ''}
+                          help={userNameError || ''}>
+                  {getFieldDecorator('email', {
+                    rules: [{
+                      type: 'email', message: 'The input is not valid E-mail!',
+                    }, {
+                      required: true, message: 'Please input your E-mail!',
+                    }],
+                  })(
                         <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="email"/>
                     )}
                 </FormItem>
-                <FormItem>
+                <FormItem validateStatus={passwordError ? 'error' : ''}
+                          help={passwordError || ''}>
                     {getFieldDecorator('password', {
                         rules: [{required: true, message: 'Please input your Password!'}],
                     })(
@@ -39,7 +57,8 @@ class NormalLoginForm extends React.Component {
                     )}
                 </FormItem>
                 <FormItem>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
+                    <Button type="primary" htmlType="submit" className="login-form-button"
+                            disabled={hasErrors(getFieldsError())}>
                         Log in
                     </Button>
 
