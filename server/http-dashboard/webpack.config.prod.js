edbba84c20f319e099b6b71975cb8c7b49d227cf
Dmitriy Dumanskiy <doom369@gmail.com>
2017-04-03 13:26:03 +0300

    #24 correct static path

--- server/http-dashboard/webpack.config.prod.js
+++ server/http-dashboard/webpack.config.prod.js
@@ -25,7 +25,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'src/main/resources/static'),
-    publicPath: '/static',
+    publicPath: '/static/',
     filename: '[name].[chunkhash].js'
   },
   plugins: [
