477d6253def0cd12f53706ec9e0bb4650349034c
Dmitriy Dumanskiy <doom369@gmail.com>
2017-04-04 12:02:17 +0300

    #15 old user field removed.

--- scripts/copy_to_knight_qa.sh
+++ scripts/copy_to_knight_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.23.4-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
+scp ../server/launcher/target/server-0.24.0-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
