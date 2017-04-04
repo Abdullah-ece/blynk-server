11c212ac745f9ace552ae7a072627bbdb8118693
Igor Gaidaichuk <i.gaidai4uk@gmail.com>
2017-04-05 01:31:12 +0200

    Make login page according to mockups #11

--- server/http-dashboard/app/components/Form/components/Field/index.js
+++ server/http-dashboard/app/components/Form/components/Field/index.js
@@ -10,6 +10,7 @@ export default class Field extends React.Component {
   renderField({displayError = true, placeholder, input, type, icon, meta: {touched, error, warning}}) {
     return (
       <Form.Item validateStatus={touched && displayError ? (error ? 'error' : warning ? 'warning' : '' ) : 'success'}
+                 className="form-field"
                  help={touched && displayError ? (error || warning ? error || warning : '' ) : ''}>
         <Input {...input} type={type} placeholder={placeholder}
                prefix={icon ? <Icon type={icon} className="form--field-icon"/> : null}/>
