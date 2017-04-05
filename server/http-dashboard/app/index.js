90c57ddd462eb08abf82d590fcfa9a290011efb1
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-06 01:56:00 +0300

    #31 Set ant locale

--- server/http-dashboard/app/index.js
+++ server/http-dashboard/app/index.js
@@ -17,22 +17,28 @@ import Store from './store';
 /* services */
 import {RouteGuestOnly, RouteAuthorizedOnly} from 'services/Login';
 
+/* vendor */
+import {LocaleProvider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+
 Store().then((store) => {
 
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={hashHistory}>
-        <Route component={Layout}>
-          <Route component={UserLayout} onEnter={RouteAuthorizedOnly(store)}>
-            <Route path="/account" component={MyAccount}/>
-            <Route path="/organization-settings" component={OrganizationSettings}/>
+      <LocaleProvider locale={enUS}>
+        <Router history={hashHistory}>
+          <Route component={Layout}>
+            <Route component={UserLayout} onEnter={RouteAuthorizedOnly(store)}>
+              <Route path="/account" component={MyAccount}/>
+              <Route path="/organization-settings" component={OrganizationSettings}/>
+            </Route>
+            <Route onEnter={RouteGuestOnly(store)}>
+              <Route path="/login" component={Login}/>
+            </Route>
           </Route>
-          <Route onEnter={RouteGuestOnly(store)}>
-            <Route path="/login" component={Login}/>
-          </Route>
-        </Route>
-        <Redirect from="*" to="/login"/>
-      </Router>
+          <Redirect from="*" to="/login"/>
+        </Router>
+      </LocaleProvider>
     </Provider>,
     document.getElementById('app')
   );
