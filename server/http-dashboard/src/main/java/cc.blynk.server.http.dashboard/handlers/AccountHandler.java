f132dff790fa5ae2b6282d2e68ffd33e2a9b37ff
Dmitriy Dumanskiy <doom369@gmail.com>
2017-04-06 18:49:13 +0300

    #52 reset pass button API support

--- server/http-dashboard/src/main/java/cc.blynk.server.http.dashboard/handlers/AccountHandler.java
+++ server/http-dashboard/src/main/java/cc.blynk.server.http.dashboard/handlers/AccountHandler.java
@@ -5,14 +5,20 @@ import cc.blynk.core.http.MediaType;
 import cc.blynk.core.http.Response;
 import cc.blynk.core.http.annotation.*;
 import cc.blynk.server.Holder;
+import cc.blynk.server.api.http.pojo.TokenUser;
+import cc.blynk.server.api.http.pojo.TokensPool;
+import cc.blynk.server.core.BlockingIOProcessor;
 import cc.blynk.server.core.dao.HttpSession;
 import cc.blynk.server.core.dao.SessionDao;
 import cc.blynk.server.core.dao.UserDao;
 import cc.blynk.server.core.model.auth.User;
+import cc.blynk.server.notifications.mail.MailWrapper;
+import cc.blynk.utils.FileLoaderUtil;
+import cc.blynk.utils.TokenGeneratorUtil;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 
-import static cc.blynk.core.http.Response.ok;
+import static cc.blynk.core.http.Response.*;
 
 /**
  * The Blynk Project.
@@ -23,16 +29,28 @@ import static cc.blynk.core.http.Response.ok;
 @ChannelHandler.Sharable
 public class AccountHandler extends BaseHttpHandler {
 
+    private static final String RESET_PASS_STATIC_PATH = "static/reset/";
     private final UserDao userDao;
+    private final String resetPassUrl;
+    private final String emailBody;
+    private final MailWrapper mailWrapper;
+    private final BlockingIOProcessor blockingIOProcessor;
+    private final TokensPool tokensPool;
 
     public AccountHandler(Holder holder, String rootPath) {
         super(holder, rootPath);
         this.userDao = holder.userDao;
+
+        this.resetPassUrl = "https://" + holder.props.getProperty("reset-pass.host") + "/landing?token=";
+        this.mailWrapper = holder.mailWrapper;
+        this.emailBody = FileLoaderUtil.readFileAsString(RESET_PASS_STATIC_PATH + "reset-email.html");
+        this.blockingIOProcessor = holder.blockingIOProcessor;
+        this.tokensPool = new TokensPool(60 * 60 * 1000);
     }
 
     @GET
     @Path("")
-    public Response getOwnProfile(@Context ChannelHandlerContext ctx) {
+    public Response get(@Context ChannelHandlerContext ctx) {
         HttpSession httpSession = ctx.channel().attr(SessionDao.userSessionAttributeKey).get();
         return ok(httpSession.user);
     }
@@ -40,10 +58,42 @@ public class AccountHandler extends BaseHttpHandler {
     @POST
     @Consumes(value = MediaType.APPLICATION_JSON)
     @Path("")
-    public Response updateOwnProfile(User updatedUser) {
+    public Response update(User updatedUser) {
         User existingUser = userDao.getByName(updatedUser.email, updatedUser.appName);
         existingUser.update(updatedUser);
         return ok(existingUser);
+    }
+
+    @POST
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Path("/resetPass")
+    public Response resetPath(@Context ChannelHandlerContext ctx) {
+        HttpSession httpSession = ctx.channel().attr(SessionDao.userSessionAttributeKey).get();
+
+        String token = TokenGeneratorUtil.generateNewToken();
+        log.info("{} trying to reset pass.", httpSession.user.email);
+
+        String email = httpSession.user.email;
+
+        TokenUser userToken = new TokenUser(email, httpSession.user.appName);
+        tokensPool.addToken(token, userToken);
+        String message = emailBody.replace("{RESET_URL}", resetPassUrl + token);
+        log.info("Sending token to {} address", email);
+
+        blockingIOProcessor.execute(() -> {
+            Response response;
+            try {
+                mailWrapper.sendHtml(email, "Password reset request.", message);
+                log.info("{} mail sent.", email);
+                response = ok("Email was sent.");
+            } catch (Exception e) {
+                log.info("Error sending mail for {}. Reason : {}", email, e.getMessage());
+                response = badRequest("Error sending reset email.");
+            }
+            ctx.writeAndFlush(response);
+        });
+
+        return noResponse();
     }
 
 }
