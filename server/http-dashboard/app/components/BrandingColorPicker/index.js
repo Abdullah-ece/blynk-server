882fdfa74dccca220ae388caf7d4dcd5e75f4f65
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-06 06:23:35 +0300

    #32 Organization Branding

---
+++ server/http-dashboard/app/components/BrandingColorPicker/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import {SketchPicker} from 'react-color';
+
+import classNames from 'classnames';
+
+import './styles.scss';
+
+class BrandingColorPicker extends React.Component {
+
+  static propTypes = {
+    title: React.PropTypes.string
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      color: '',
+      displayColorPicker: false
+    };
+  }
+
+  displayColorPicker() {
+    this.setState({
+      displayColorPicker: true
+    });
+  }
+
+  hideColorPicker() {
+    this.setState({
+      displayColorPicker: false
+    });
+  }
+
+  handleColorChange(color) {
+    this.setState({
+      color: color.hex.replace('#', '')
+    });
+  }
+
+  render() {
+
+    const pickerClass = classNames({
+      'branding-color-picker-box-color-code': true,
+      'placeholder': !this.state.color
+    });
+
+    return (
+      <div className="branding-color-picker">
+        { this.state.displayColorPicker &&
+        <div className="branding-color-picker-color-preview-overlay" onClick={this.hideColorPicker.bind(this)}/> }
+        <div className="branding-color-picker-name">
+          { this.props.title }
+        </div>
+        <div className="branding-color-picker-box">
+          <div className={pickerClass}>
+            { this.state.color || 'Choose' }
+          </div>
+          <div className="branding-color-picker-color-preview">
+            { this.state.displayColorPicker &&
+            <SketchPicker color={this.state.color} onChange={this.handleColorChange.bind(this)}/>}
+            { !this.state.color &&
+            <div className="branding-color-picker-color-preview-choose" onClick={this.displayColorPicker.bind(this)}/>
+            }
+            { this.state.color &&
+            <div className="branding-color-picker-color-preview-specific" onClick={this.displayColorPicker.bind(this)}
+                 style={{background: '#' + this.state.color}}/> }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BrandingColorPicker;
--- server/http-dashboard/app/scenes/UserProfile/scenes/OrganizationSettings/components/OrganizationBranding/index.js
+++ server/http-dashboard/app/scenes/UserProfile/scenes/OrganizationSettings/components/OrganizationBranding/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './styles.scss';
 import LogoUploader from "components/LogoUploader/index";
+import BrandingColorPicker from "components/BrandingColorPicker/index";
 
 export default class OrganizationBranding extends React.Component {
   render() {
     return (
-      <div>
+      <div className="user-profile--organization-settings--organization-branding">
         <LogoUploader/>
+        <div className="user-profile--organization-settings--organization-branding-colors">
+          <BrandingColorPicker title="primary color"/>
+          <BrandingColorPicker title="secondary color"/>
+        </div>
       </div>
     );
   }
