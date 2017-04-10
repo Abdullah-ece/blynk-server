a0b5ef2e93d67e1c7eceb6bf7887356a69b4d131
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-10 16:49:49 +0300

    #45 Button & Input styleguide integration

--- server/http-dashboard/app/scenes/StyleGuide/index.js
+++ server/http-dashboard/app/scenes/StyleGuide/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Button} from 'antd';
+import {Button, Input, Checkbox} from 'antd';
 
 import './styles.scss';
 
@@ -8,7 +8,39 @@ class StyleGuide extends React.Component {
   render() {
     return (
       <div className="style-guide">
-        <Button type="primary">Button</Button>
+        <div className="style-guide-element">
+          <Button type="primary">Button</Button>
+        </div>
+        <div className="style-guide-element">
+          <Button type="primary" icon="plus">Button</Button>
+        </div>
+        <div className="style-guide-element">
+          <Button>Button</Button>
+        </div>
+        <div className="style-guide-element">
+          <Button type="danger">Button</Button>
+        </div>
+        <div className="style-guide-element">
+          <Button type="dashed" icon="plus">Add Meta data</Button>
+        </div>
+        <div className="style-guide-element">
+          <Input placeholder="Label" style={{width: 168}}/>
+        </div>
+        <div className="style-guide-element">
+          <Input placeholder="Label" style={{width: 168}} disabled/>
+        </div>
+        <div className="style-guide-element has-error">
+          <Input placeholder="Label" style={{width: 168}}/>
+        </div>
+        <div className="style-guide-element">
+          <Checkbox/>
+        </div>
+        <div className="style-guide-element">
+          <Checkbox disabled/>
+        </div>
+        <div className="style-guide-element has-error">
+          <Checkbox/>
+        </div>
       </div>
     );
   }
