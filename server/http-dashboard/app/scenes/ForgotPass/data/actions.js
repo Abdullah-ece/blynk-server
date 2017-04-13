66535748f8d0d13c497551358aece1411eb0fc3a
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-13 18:15:10 +0300

    #121 Fix forgot pass

--- server/http-dashboard/app/scenes/ForgotPass/data/actions.js
+++ server/http-dashboard/app/scenes/ForgotPass/data/actions.js
@@ -1,8 +1,12 @@
+import {transformJsonToFormUrlEncoded} from 'services/Form';
+
 export function ForgotPass(data) {
   return {
-    type: 'API_RESET',
+    type: 'API_SEND_RESET_PASS',
     payload: {
       request: {
+        transformRequest: transformJsonToFormUrlEncoded,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         method: 'post',
         url: '/sendResetPass',
         data
