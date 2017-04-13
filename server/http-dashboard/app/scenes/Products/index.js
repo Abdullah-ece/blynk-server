7cc8a5f190b10991acc19235ec38f1fa03804879
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-13 22:47:35 +0300

    #108 Products Index Page

--- server/http-dashboard/app/scenes/Products/index.js
+++ server/http-dashboard/app/scenes/Products/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import {Button} from 'antd';
 import './styles.less';
 
 class ProductsIndex extends React.Component {
   render() {
     return (
-      <div>Products Index</div>
+      <div className="products">
+
+        <div className="products-no-items">
+          <div className="products-no-items-title">Start by creating your first product</div>
+          <div className="products-no-items-description">Product is a digital model of a physical object. It is used in
+            Blynk platform as a template to be assigned to devices. Learn more
+          </div>
+          <div className="products-no-items-action">
+            <Button icon="plus" type="primary">Create New Product</Button>
+          </div>
+        </div>
+
+      </div>
     );
   }
 }
