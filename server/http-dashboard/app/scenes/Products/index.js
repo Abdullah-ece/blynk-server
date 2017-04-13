2ef0a0cc89a85e2cfb5a2a9f34a9108d1edebf23
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-13 22:49:15 +0300

    #108 Remove Learn More link

--- server/http-dashboard/app/scenes/Products/index.js
+++ server/http-dashboard/app/scenes/Products/index.js
@@ -10,7 +10,7 @@ class ProductsIndex extends React.Component {
         <div className="products-no-items">
           <div className="products-no-items-title">Start by creating your first product</div>
           <div className="products-no-items-description">Product is a digital model of a physical object. It is used in
-            Blynk platform as a template to be assigned to devices. Learn more
+            Blynk platform as a template to be assigned to devices. {/*Lean More @todo add link when have this page*/}
           </div>
           <div className="products-no-items-action">
             <Button icon="plus" type="primary">Create New Product</Button>
