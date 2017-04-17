6d5b7814230e0f42cd06636986b91f5ba8e676ed
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-17 16:14:35 +0300

    #110 Service for fields validation

--- server/http-dashboard/app/services/Form/index.js
+++ server/http-dashboard/app/services/Form/index.js
@@ -4,3 +4,23 @@ export const transformJsonToFormUrlEncoded = (data) => {
     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(data[p]));
   return str.join("&");
 };
+
+export function formHasError(fieldsErrors) {
+  const errors = [];
+  Object.keys(fieldsErrors).forEach((key) => {
+    if (fieldsErrors[key])
+      errors.push(fieldsErrors[key]);
+  });
+
+  return !!errors.length;
+}
+
+export function getFormFirstError(fieldsErrors) {
+  const errors = [];
+  Object.keys(fieldsErrors).forEach((key) => {
+    if (fieldsErrors[key])
+      errors.push(fieldsErrors[key]);
+  });
+
+  return errors.length ? errors[0] : null;
+}
