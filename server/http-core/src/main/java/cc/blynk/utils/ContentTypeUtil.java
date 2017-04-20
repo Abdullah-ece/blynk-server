e6135e9f343780c33e53ce52f671b1c8bd673dbc
Dmitriy Dumanskiy <doom369@gmail.com>
2017-04-20 15:31:18 +0300

    Revert: #160 wrong content type for svg (1ac92ede617eaf9d6161806d0d113acf39ec098d)

---
+++ server/http-core/src/main/java/cc/blynk/utils/ContentTypeUtil.java
@@ -0,0 +1,30 @@
+package cc.blynk.utils;
+
+/**
+ * The Blynk Project.
+ * Created by Dmitriy Dumanskiy.
+ * Created on 07.07.16.
+ */
+public class ContentTypeUtil {
+
+    public static String getContentType(String fileName) {
+        if (fileName.endsWith(".ico")) {
+            return "image/x-icon";
+        } else if (fileName.endsWith(".js")) {
+            return "application/javascript";
+        } else if (fileName.endsWith(".css")) {
+            return "text/css";
+        } else if (fileName.endsWith(".png")) {
+            return "image/png";
+        } else if (fileName.endsWith(".jpg")) {
+            return "image/jpg";
+        } else if (fileName.endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else if (fileName.endsWith(".gz")) {
+            return "application/x-gzip";
+        }
+
+        return "text/html";
+    }
+
+}
