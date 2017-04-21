54fd3c9e4f645cf383f111f6fe5a0655d96f3367
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-21 22:49:35 +0300

    #110 Add additional metadata fields dropdown, override default styles

--- server/http-dashboard/app/scenes/Products/scenes/Create/components/AddNewMetadataField/index.js
+++ server/http-dashboard/app/scenes/Products/scenes/Create/components/AddNewMetadataField/index.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import {Metadata} from 'services/Products';
-import {Button, /*Menu, Dropdown, Icon*/} from 'antd';
+import {Button, Menu, Dropdown, Icon} from 'antd';
 import './styles.less';
-
-// const menu = (
-//   <Menu>
-//     <Menu.Item>
-//       Text
-//     </Menu.Item>
-//     <Menu.Item>
-//      Number
-//     </Menu.Item>
-//     <Menu.Item>
-//      Unit
-//     </Menu.Item>
-//     <Menu.Item>
-//       Cost
-//     </Menu.Item>
-//     <Menu.Item>
-//       Switch
-//     </Menu.Item>
-//     <Menu.Item>
-//       List
-//     </Menu.Item>
-//   </Menu>
-// );
 
 class AddNewMetadataField extends React.Component {
 
   static propTypes = {
     onFieldAdd: React.PropTypes.func
   };
+
+  menu = (
+    <Menu selectable={false} onClick={this.handleMenuClick.bind(this)}>
+      <Menu.Item key={Metadata.Fields.COST}>
+        Cost
+      </Menu.Item>
+      <Menu.Item key={Metadata.Fields.COORDINATES}>
+        Coordinates
+      </Menu.Item>
+    </Menu>
+  );
+
+  handleMenuClick({key}) {
+    this.addField(key);
+  }
+
+  addField(type) {
+    this.props.onFieldAdd({
+      type: type
+    });
+  }
 
   addTextField() {
     this.props.onFieldAdd({
@@ -87,22 +85,22 @@ class AddNewMetadataField extends React.Component {
         <div className="products-add-new-metadata-field-fields">
           <Button type="dashed" onClick={this.addTextField.bind(this)}>Text</Button>
           <Button type="dashed" onClick={this.addNumberField.bind(this)}>Number</Button>
+          <Button type="dashed" onClick={this.addUnitField.bind(this)}>Unit</Button>
           <Button type="dashed" onClick={this.addTimeField.bind(this)}>Time</Button>
           <Button type="dashed" onClick={this.addShiftField.bind(this)}>Shift</Button>
-          <Button type="dashed" onClick={this.addCostField.bind(this)}>Cost</Button>
-          <Button type="dashed" onClick={this.addCoordinatesField.bind(this)}>Coordinates</Button>
-          <Button type="dashed" onClick={this.addUnitField.bind(this)}>Unit</Button>
-          <Button type="dashed" onClick={this.addContactField.bind(this)}>Contact</Button>
+          {/*<Button type="dashed" onClick={this.addCostField.bind(this)}>Cost</Button>*/}
+          {/*<Button type="dashed" onClick={this.addCoordinatesField.bind(this)}>Coordinates</Button>*/}
+          {/*<Button type="dashed" onClick={this.addContactField.bind(this)}>Contact</Button>*/}
           {/*<Button type="dashed">Contact</Button>*/}
         </div>
         {/* @todo uncomment when add additional types of metadata*/}
-        {/*<div className="products-add-new-metadata-field-other-fields">*/}
-        {/*<Dropdown overlay={menu}>*/}
-        {/*<a className="ant-dropdown-link" href="#">*/}
-        {/*Other Metadata Types <Icon type="down" />*/}
-        {/*</a>*/}
-        {/*</Dropdown>*/}
-        {/*</div>*/}
+        <div className="products-add-new-metadata-field-other-fields">
+          <Dropdown overlay={this.menu}>
+            <a className="ant-dropdown-link" href="javascript:void(0);">
+              Other Metadata Types <Icon type="down"/>
+            </a>
+          </Dropdown>
+        </div>
       </div>
     );
   }
