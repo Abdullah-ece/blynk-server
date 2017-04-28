2dd725982764292a97d3a745c320713be5974d96
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-28 11:11:36 +0300

    #193 Product Management component responsible for product operations

---
+++ server/http-dashboard/app/scenes/Products/components/ProductManage/components/Info/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import InfoForm from './components/InfoForm';
+
+class Info extends React.Component {
+
+  static propTypes = {
+    onChange: React.PropTypes.func,
+    values: React.PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.invalid = false;
+  }
+
+  onChange(values) {
+    this.props.onChange(values);
+  }
+
+  getInitialValues() {
+    const values = {
+      boardType: this.props.values.boardType,
+      connectionType: this.props.values.connectionType,
+      name: this.props.values.name,
+      description: this.props.values.description,
+      logoUrl: this.props.values.logoUrl
+    };
+    return values;
+  }
+
+  render() {
+
+    if (!this.props.values.name)
+      return null;
+
+    return (
+      <InfoForm onChange={this.onChange.bind(this)} initialValues={this.getInitialValues()}/>
+    );
+  }
+}
+
+export default Info;
---
+++ server/http-dashboard/app/scenes/Products/components/ProductManage/components/Metadata/index.js
@@ -0,0 +1,284 @@
+import React from 'react';
+import AddNewMetadataField from "scenes/Products/components/AddNewMetadataField";
+import {Metadata as MetadataService} from 'services/Products';
+import Metadata from "scenes/Products/components/Metadata";
+import {MetadataRolesDefault} from 'services/Roles';
+const MetadataFields = Metadata.Fields;
+import _ from 'lodash';
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+class ProductMetadata extends React.Component {
+
+  static propTypes = {
+
+    fields: React.PropTypes.array,
+
+    onFieldChange: React.PropTypes.func,
+    onFieldsChange: React.PropTypes.func
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleChangeField(values, dispatch, props) {
+
+    const field = _.find(this.props.fields, {id: props.id});
+
+    this.props.onFieldChange({
+      ...field,
+      values
+    });
+  }
+
+  SortableItem = SortableElement(({value}) => {
+
+    const field = value;
+
+    const props = {
+      id: field.id,
+      key: field.id,
+      form: `metadatafield${field.id}`,
+      onChange: this.handleChangeField.bind(this),
+      validate: this.metadataFieldValidation.bind(this),
+      onDelete: this.handleDeleteField.bind(this),
+      onClone: this.handleCloneField.bind(this)
+    };
+
+    if (field.type === MetadataService.Fields.TEXT) {
+      return (
+        <MetadataFields.TextField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            value: field.values.value,
+            role: field.values.role
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.NUMBER) {
+      return (
+        <MetadataFields.NumberField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            value: field.values.value,
+            role: field.values.role
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.COST) {
+      return (
+        <MetadataFields.CostField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            value: field.values.value,
+            currency: field.values.currency,
+            role: field.values.role
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.TIME) {
+      return (
+        <MetadataFields.TimeField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            time: field.values.time,
+            role: field.values.role
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.SHIFT) {
+      return (
+        <MetadataFields.ShiftField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            from: field.values.from,
+            role: field.values.role,
+            to: field.values.to
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.SWITCH) {
+      return (
+        <MetadataFields.SwitchField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            from: field.values.from,
+            to: field.values.to
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.DATE) {
+      return (
+        <MetadataFields.DateField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            value: field.values.value
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.COORDINATES) {
+      return (
+        <MetadataFields.CoordinatesField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            lat: field.values.lat,
+            role: field.values.role,
+            lon: field.values.lon
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.UNIT) {
+      return (
+        <MetadataFields.UnitField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            value: field.values.value,
+            role: field.values.role,
+            units: field.values.units
+          }}
+        />
+      );
+    }
+
+    if (field.type === MetadataService.Fields.CONTACT) {
+      return (
+        <MetadataFields.ContactField
+          {...props}
+          initialValues={{
+            name: field.values.name,
+            role: field.values.role,
+            value: field.values.value
+          }}
+        />
+      );
+    }
+
+  });
+
+  SortableList = SortableContainer(({items}) => {
+    return (
+      <Metadata.ItemsList>
+        {items.map((value, index) => {
+          return (
+            <this.SortableItem key={`item-${value.id}`} index={index} value={value}/>
+          );
+        })}
+      </Metadata.ItemsList>
+    );
+  });
+
+  metadataFieldValidation(values, props) {
+    const errors = {};
+
+    this.props.fields.forEach((field) => {
+      if (field.values.name === values.name && Number(props.id) !== Number(field.id)) {
+        errors.name = 'Name should be unique';
+      }
+    });
+
+    return errors;
+  }
+
+  handleCloneField(id) {
+
+    const cloned = _.find(this.props.fields, {id: id});
+
+    const nextId = this.props.fields.reduce((acc, value) => (
+        acc < value.id ? value.id : acc
+      ), this.props.fields.length ? this.props.fields[0].id : 0) + 1;
+
+    const fields = [
+      ...this.props.fields,
+      {
+        ...cloned,
+        id: nextId,
+        values: {
+          ...cloned.values,
+          name: `${cloned.values.name} Copy`
+        }
+      }
+    ];
+
+    const originalIndex = _.findIndex(fields, {id: id});
+
+    this.props.onFieldsChange(
+      arrayMove(fields, fields.length - 1, originalIndex + 1)
+    );
+  }
+
+  addMetadataField(params) {
+
+    const nextId = this.props.fields.reduce((acc, value) => (
+        acc < value.id ? value.id : acc
+      ), this.props.fields.length ? this.props.fields[0].id : 0) + 1;
+
+
+    this.props.onFieldsChange([
+      ...this.props.fields,
+      {
+        id: nextId,
+        type: params.type,
+        values: {
+          role: MetadataRolesDefault,
+          name: '',
+          value: '',
+          ...params.values
+        }
+      }
+    ]);
+  }
+
+  handleDeleteField(key) {
+    this.props.onFieldsChange(this.props.fields.filter((field) => field.id !== key));
+  }
+
+  onSortEnd({oldIndex, newIndex}) {
+
+    this.props.onFieldsChange(
+      arrayMove(this.props.fields, oldIndex, newIndex)
+    );
+
+  }
+
+  render() {
+
+    return (
+      <div>
+        { this.props.fields && this.props.fields.length && (
+          <this.SortableList items={this.props.fields} onSortEnd={this.onSortEnd.bind(this)}
+                             useDragHandle={true}
+                             lockAxis="y"
+                             helperClass="product-metadata-item-drag-active"/>) || null
+        }
+
+        <AddNewMetadataField onFieldAdd={this.addMetadataField.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default ProductMetadata;
