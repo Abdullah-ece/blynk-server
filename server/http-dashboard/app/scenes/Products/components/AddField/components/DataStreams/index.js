84fd7366f87e52dd989c0dcfd25944480d0f5e2b
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-04-30 14:09:58 +0300

    #213 Add DataStream component

--- server/http-dashboard/app/scenes/Products/components/AddField/components/DataStreams/index.js
+++ server/http-dashboard/app/scenes/Products/components/AddField/components/DataStreams/index.js
@@ -1,47 +1,19 @@
-import Base from '../Base';
-import {Metadata, Currency} from 'services/Products';
+import React from 'react';
+import {Button} from 'antd';
+export default class AddDataStreamsField extends React.Component {
 
-export default class AddMetadataFields extends Base {
-
-  title = 'Metadata';
-
-  fields = [
-    {
-      type: Metadata.Fields.TEXT,
-      title: 'Text'
-    },
-    {
-      type: Metadata.Fields.NUMBER,
-      title: 'Number'
-    },
-    {
-      type: Metadata.Fields.UNIT,
-      title: 'Unit'
-    },
-    {
-      type: Metadata.Fields.TIME,
-      title: 'Time'
-    },
-    {
-      type: Metadata.Fields.SHIFT,
-      title: 'Shift'
-    },
-    {
-      type: Metadata.Fields.COST,
-      title: 'Cost'
-    },
-    {
-      type: Metadata.Fields.COORDINATES,
-      title: 'Coordinates'
-    }
-  ];
-
-  typesPredefinedValues = {
-    [Metadata.Fields.COST]: {
-      values: {
-        currency: Currency.USD.key
-      }
-    }
+  static propTypes = {
+    onFieldAdd: React.PropTypes.func
   };
+
+  render() {
+    return (
+      <div className="products-add-new-field">
+        <div className="products-add-new-field-fields">
+          <Button type="dashed" onClick={this.addField}>+ Add New Data Stream</Button>
+        </div>
+      </div>
+    );
+  }
 
 }
--- server/http-dashboard/app/scenes/Products/components/ProductManage/components/DataStreams/index.js
+++ server/http-dashboard/app/scenes/Products/components/ProductManage/components/DataStreams/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import {AddDataStreamsFields} from 'scenes/Products/components/AddField';
 
 // import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 class DataStreams extends React.Component {
 
+  addDataStreamsField() {
+    //  some there
+  }
+
   render() {
     return (
       <div>
-        Data Streams
-        {/*<AddNewMetadataField onFieldAdd={this.addMetadataField.bind(this)}/>*/}
+        <AddDataStreamsFields onFieldAdd={this.addDataStreamsField.bind(this)}/>
       </div>
     );
   }
