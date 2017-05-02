a13e29b56ad52c21b7e8836ed1cfb1b79a01f654
Dmitriy Dumanskiy <doom369@gmail.com>
2017-05-02 20:17:12 +0300

    test fixes after merge and netty version update 2

--- scripts/copy_to_knight_qa.sh
+++ scripts/copy_to_knight_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.24.2-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
+scp ../server/launcher/target/server-0.24.3-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
