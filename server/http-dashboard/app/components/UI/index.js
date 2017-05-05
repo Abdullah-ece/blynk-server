7f1b5d6a2f739d1f74b02b6e9ac26491f622f376
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-05-05 13:52:46 +0300

    #114 Contact Field UI Fix exports

--- server/http-dashboard/app/components/UI/index.js
+++ server/http-dashboard/app/components/UI/index.js
@@ -1,7 +1,13 @@
 import Form from './components/Form';
 
+export Form from './components/Form';
+export Item from './components/Form/components/Item';
+export ItemsGroup from './components/Form/components/ItemsGroup';
+
 const UI = {
-  Form: Form
+  Form: Form,
+  Item: Form.Item,
+  ItemGroup: Form.ItemsGroup
 };
 
 export default UI;
