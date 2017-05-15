631c6c0b551c4a3df1b7b33318adb651cb0b5103
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-05-15 11:28:23 +0300

    #278 Provide multipleLines for FieldStub

--- server/http-dashboard/app/scenes/Products/components/FieldStub/index.js
+++ server/http-dashboard/app/scenes/Products/components/FieldStub/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './styles.less';
-import Dotdotdot from 'react-dotdotdot'
+import Dotdotdot from 'react-dotdotdot';
 
 class FieldStub extends React.Component {
+
+  static propTypes = {
+    multipleLines: React.PropTypes.bool,
+    children: React.PropTypes.any
+  };
 
   render() {
     return (
       <div className={`product-metadata-static-field ${!this.props.children && 'no-value'}`}
            style={{wordWrap: 'break-word'}}>
-        <Dotdotdot clamp={1}>
-          { this.props.children || 'No Value' }
-        </Dotdotdot>
+        { !this.props.multipleLines && (
+          <Dotdotdot clamp={1}>
+            { this.props.children || 'No Value' }
+          </Dotdotdot>
+        ) || (
+          <p>{ this.props.children || 'No Value' }</p>
+        )}
       </div>
     );
   }
