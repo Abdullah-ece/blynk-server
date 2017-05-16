d73beb876b44572129db9d79a89bc94bee0d1d70
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-05-16 18:05:55 +0300

    #238 Fix TextField static long values

--- server/http-dashboard/app/scenes/Products/components/Metadata/components/TextField/static.js
+++ server/http-dashboard/app/scenes/Products/components/Metadata/components/TextField/static.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Input} from 'antd';
 import BaseField from '../BaseField';
 import FormItem from 'components/FormItem';
-import classnames from 'classnames';
+import FieldStub from 'scenes/Products/components/FieldStub';
 
 class TextField extends BaseField.Static {
 
@@ -25,11 +25,6 @@ class TextField extends BaseField.Static {
 
   component() {
 
-    const valueClassNames = classnames({
-      'product-metadata-static-field': true,
-      'no-value': !this.props.value
-    });
-
     return (
       <FormItem offset={false}>
         <FormItem.TitleGroup>
@@ -38,12 +33,12 @@ class TextField extends BaseField.Static {
         </FormItem.TitleGroup>
         <FormItem.Content input>
           <Input.Group compact>
-            <div className="product-metadata-static-field">
+            <FieldStub style={{width: '50%'}}>
               {this.props.name}
-            </div>
-            <div className={valueClassNames}>
-              {this.props.value || this.DEFAULT_VALUE}
-            </div>
+            </FieldStub>
+            <FieldStub style={{width: '50%'}}>
+              {this.props.value}
+            </FieldStub>
           </Input.Group>
         </FormItem.Content>
       </FormItem>
