e90ecd05ff28fc1943306cd78136631c02e8f16f
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-05-16 09:32:28 +0300

    #285 Fix notifications select text

--- server/http-dashboard/app/scenes/Products/components/Events/components/Base/notifications.js
+++ server/http-dashboard/app/scenes/Products/components/Events/components/Base/notifications.js
@@ -29,7 +29,7 @@ class Notifications extends React.Component {
               placeholder="Select contact"
               allowClear={true}
               filterOption={(value, option) => SimpleMatch(value, option.key)}
-              notFoundContent={!props.options.length ? 'No any metadata Contact field with Email' : 'No any field matches your request'}>
+              notFoundContent={!props.options.length ? 'Add a "Contact" type Metadata to enable notifications' : 'No any field matches your request'}>
         { props.options }
       </Select>
     );
