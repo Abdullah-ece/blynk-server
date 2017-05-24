8875e963c3265bf9c406fd3ce6037a4033c51c7f
Dmitriy Dumanskiy <doom369@gmail.com>
2017-05-24 11:11:45 +0300

    merge from upstream

--- scripts/copy_to_knight_qa.sh
+++ scripts/copy_to_knight_qa.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.24.5-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
+scp ../server/launcher/target/server-0.24.6-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
