3048ecce309513200219b97b4f38b78280d2c485
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-02 11:48:00 +0300

    #365 Add props inheritence for Section

--- server/http-dashboard/app/components/Section/index.js
+++ server/http-dashboard/app/components/Section/index.js
@@ -4,12 +4,12 @@ import './styles.less';
 class Section extends React.Component {
   static propTypes = {
     title: React.PropTypes.string,
-    children: React.PropTypes.any
+    children: React.PropTypes.any,
   };
 
   render() {
     return (
-      <div className="content-section">
+      <div className="content-section" {...this.props}>
         <div className="content-section--name">{this.props.title}</div>
         {this.props.children}
       </div>
