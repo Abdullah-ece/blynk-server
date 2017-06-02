46f2ece0fda43fca5443179884d3087626450113
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-02 19:23:00 +0300

    #365 Device Metadata Item edit button & Metadata Text

--- server/http-dashboard/app/components/DeviceMetadata/components/Item/index.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Item/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Row, Col, Button, Icon} from 'antd';
 import './styles.less';
 
 class Item extends React.Component {
 
+  static propTypes = {
+    children: React.PropTypes.any
+  };
+
   render() {
     return (
       <div className="device-metadata--item">
-        Some is there
+        <Row type="flex">
+          <Col span={14}>
+            {this.props.children}
+          </Col>
+          <Col span={10} className="device-metadata--item-edit">
+            <Button type="primary"><Icon type="edit"/>Edit</Button>
+            {/*<div className="device-metadata--item-edit-no-permission">No permissions to edit</div>*/}
+          </Col>
+        </Row>
       </div>
     );
   }
