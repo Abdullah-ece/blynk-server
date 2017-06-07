fc86b00bff1f8b936cba58b3ccac0cf4850937b2
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-07 14:24:00 +0300

    #360 Device Metadata Contact Modal

--- server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {reduxForm} from 'redux-form';
 import {MetadataField} from 'components/Form';
+import {Row, Col} from 'antd';
+
+import './styles.less';
 
 @reduxForm({
   form: 'deviceMetadataEdit'
@@ -9,8 +12,39 @@ class ContactModal extends React.Component {
 
   render() {
     return (
-      <div>
-        <MetadataField placeholder="Value" name="value"/>
+      <div className="device-metadata--contact-modal">
+        <Row>
+          <Col span={12}>
+            <MetadataField name="firstName" placeholder="First name"/>
+          </Col>
+          <Col span={12}>
+            <MetadataField name="streetAddress" placeholder="Street Address"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <MetadataField name="lastName" placeholder="Last name"/>
+          </Col>
+          <Col span={12}>
+            <MetadataField name="city" placeholder="City"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <MetadataField name="email" placeholder="E-mail"/>
+          </Col>
+          <Col span={12}>
+            <MetadataField name="state" placeholder="State"/>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <MetadataField name="phone" placeholder="Phone"/>
+          </Col>
+          <Col span={12}>
+            <MetadataField name="zip" placeholder="ZIP"/>
+          </Col>
+        </Row>
       </div>
     );
   }
