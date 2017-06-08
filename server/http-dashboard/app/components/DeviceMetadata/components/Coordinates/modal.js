1bb1c5e6f11af4626bc067a9d007c79e275a0015
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-08 04:56:04 +0300

    #363 Device Metadata Coordinates Modal

--- server/http-dashboard/app/components/DeviceMetadata/components/Coordinates/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Coordinates/modal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {reduxForm} from 'redux-form';
 import {MetadataField} from 'components/Form';
 import Validation from 'services/Validation';
+import {Input} from 'antd';
 
 @reduxForm({
   form: 'deviceMetadataEdit'
@@ -11,7 +12,10 @@ class CoordinatesModal extends React.Component {
   render() {
     return (
       <div>
-        <MetadataField placeholder="Value" name="value" validate={[Validation.Rules.number]}/>
+        <Input.Group compact>
+          <MetadataField placeholder="Latitude" name="lat" validate={[Validation.Rules.latitude]}/>
+          <MetadataField placeholder="Longitude" name="lon" validate={[Validation.Rules.longitude]}/>
+        </Input.Group>
       </div>
     );
   }
