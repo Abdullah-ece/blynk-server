5c6111c5d662bde076de8f1613e9573980af83ab
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-09 12:36:31 +0300

    #353 Add separation between devices cells. Fix hover effect

--- server/http-dashboard/app/scenes/Devices/components/DevicesList/components/DeviceItem/index.js
+++ server/http-dashboard/app/scenes/Devices/components/DevicesList/components/DeviceItem/index.js
@@ -20,20 +20,22 @@ class DeviceItem extends React.Component {
 
     return (
       <div className={className} onClick={this.props.onClick}>
-        <div className="navigation-devices-list-item-name">
-          <Dotdotdot clamp={1}>{this.props.device.get('name')}</Dotdotdot>
-        </div>
-        <div className="navigation-devices-list-item-product-name">
-          <Dotdotdot clamp={1}>{this.props.device.has('product.name') && this.props.device.getIn('product.name') || (
-            <i>No Product Name</i>) }</Dotdotdot>
-        </div>
-        <div className="navigation-devices-list-item-events">
-          { this.props.device.has('critical') && (
-            <Badge count={this.props.device.get('critical')} className="critical"/>
-          )}
-          { !this.props.device.has('critical') && this.props.device.has('warning') && (
-            <Badge count={this.props.device.get('warning')} className="warning"/>
-          )}
+        <div className="navigation-devices-list-item-inner">
+          <div className="navigation-devices-list-item-name">
+            <Dotdotdot clamp={1}>{this.props.device.get('name')}</Dotdotdot>
+          </div>
+          <div className="navigation-devices-list-item-product-name">
+            <Dotdotdot clamp={1}>{this.props.device.has('product.name') && this.props.device.getIn('product.name') || (
+              <i>No Product Name</i>) }</Dotdotdot>
+          </div>
+          <div className="navigation-devices-list-item-events">
+            { this.props.device.has('critical') && (
+              <Badge count={this.props.device.get('critical')} className="critical"/>
+            )}
+            { !this.props.device.has('critical') && this.props.device.has('warning') && (
+              <Badge count={this.props.device.get('warning')} className="warning"/>
+            )}
+          </div>
         </div>
       </div>
     );
