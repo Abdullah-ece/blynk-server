8712911f7d8ac3070001ce3fda2a4449c5429237
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-10 19:00:51 +0300

    #393 Provide title for Range metadata field

--- server/http-dashboard/app/components/DeviceMetadata/components/Range/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Range/modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {reduxForm} from 'redux-form';
-import {Input} from 'antd';
+import {ItemsGroup, Item} from 'components/UI';
 import {MetadataTime} from 'components/Form';
 
 @reduxForm({
@@ -11,10 +11,14 @@ class RangeModal extends React.Component {
   render() {
     return (
       <div>
-        <Input.Group compact>
-          <MetadataTime name="from" type="text" timeFormat="HH:mm" placeholder="00:00"/>
-          <MetadataTime name="to" type="text" timeFormat="HH:mm" placeholder="00:00"/>
-        </Input.Group>
+        <ItemsGroup>
+          <Item label="From" offset="normal">
+            <MetadataTime name="from" type="text" timeFormat="HH:mm" placeholder="00:00" style={{width: '100%'}}/>
+          </Item>
+          <Item label="To" offset="normal">
+            <MetadataTime name="to" type="text" timeFormat="HH:mm" placeholder="00:00" style={{width: '100%'}}/>
+          </Item>
+        </ItemsGroup>
       </div>
     );
   }
