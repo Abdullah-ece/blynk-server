f0de80bc75ee2def893d027ef4d5aa114c97da32
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-10 19:11:53 +0300

    #393 Provide title for Contact metadata field

--- server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {reduxForm} from 'redux-form';
-import {MetadataField} from 'components/Form';
+import {Item, Input} from 'components/UI';
 import {Row, Col} from 'antd';
 
 import './styles.less';
@@ -15,34 +15,50 @@ class ContactModal extends React.Component {
       <div className="device-metadata--contact-modal">
         <Row>
           <Col span={12}>
-            <MetadataField name="firstName" placeholder="First name"/>
+            <Item label="First Name" offset="normal">
+              <Input name="firstName" placeholder="First name"/>
+            </Item>
           </Col>
           <Col span={12}>
-            <MetadataField name="streetAddress" placeholder="Street Address"/>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={12}>
-            <MetadataField name="lastName" placeholder="Last name"/>
-          </Col>
-          <Col span={12}>
-            <MetadataField name="city" placeholder="City"/>
+            <Item label="Street Address" offset="normal">
+              <Input name="streetAddress" placeholder="Street Address"/>
+            </Item>
           </Col>
         </Row>
         <Row>
           <Col span={12}>
-            <MetadataField name="email" placeholder="E-mail"/>
+            <Item label="Last name" offset="normal">
+              <Input name="lastName" placeholder="Last name"/>
+            </Item>
           </Col>
           <Col span={12}>
-            <MetadataField name="state" placeholder="State"/>
+            <Item label="City" offset="normal">
+              <Input name="city" placeholder="City"/>
+            </Item>
           </Col>
         </Row>
         <Row>
           <Col span={12}>
-            <MetadataField name="phone" placeholder="Phone"/>
+            <Item label="E-mail" offset="normal">
+              <Input name="email" placeholder="E-mail"/>
+            </Item>
           </Col>
           <Col span={12}>
-            <MetadataField name="zip" placeholder="ZIP"/>
+            <Item label="State" offset="normal">
+              <Input name="state" placeholder="State"/>
+            </Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Item label="Phone" offset="normal">
+              <Input name="phone" placeholder="Phone"/>
+            </Item>
+          </Col>
+          <Col span={12}>
+            <Item label="ZIP" offset="normal">
+              <Input name="zip" placeholder="ZIP"/>
+            </Item>
           </Col>
         </Row>
       </div>
