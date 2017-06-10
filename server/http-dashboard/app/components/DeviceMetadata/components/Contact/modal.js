40ec4e6385f2e31d1e361c40d37b3717613d9f2c
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-06-10 19:13:46 +0300

    #399 Make Contact Metadata Fields required

--- server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Contact/modal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {reduxForm} from 'redux-form';
 import {Item, Input} from 'components/UI';
 import {Row, Col} from 'antd';
+import Validation from 'services/Validation';
 
 import './styles.less';
 
@@ -16,48 +17,49 @@ class ContactModal extends React.Component {
         <Row>
           <Col span={12}>
             <Item label="First Name" offset="normal">
-              <Input name="firstName" placeholder="First name"/>
+              <Input name="firstName" placeholder="First name" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
           <Col span={12}>
             <Item label="Street Address" offset="normal">
-              <Input name="streetAddress" placeholder="Street Address"/>
+              <Input name="streetAddress" placeholder="Street Address" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
         </Row>
         <Row>
           <Col span={12}>
             <Item label="Last name" offset="normal">
-              <Input name="lastName" placeholder="Last name"/>
+              <Input name="lastName" placeholder="Last name" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
           <Col span={12}>
             <Item label="City" offset="normal">
-              <Input name="city" placeholder="City"/>
+              <Input name="city" placeholder="City" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
         </Row>
         <Row>
           <Col span={12}>
             <Item label="E-mail" offset="normal">
-              <Input name="email" placeholder="E-mail"/>
+              <Input name="email" placeholder="E-mail" validate={[Validation.Rules.required, Validation.Rules.email]}
+                     validateOnBlur={true}/>
             </Item>
           </Col>
           <Col span={12}>
             <Item label="State" offset="normal">
-              <Input name="state" placeholder="State"/>
+              <Input name="state" placeholder="State" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
         </Row>
         <Row>
           <Col span={12}>
             <Item label="Phone" offset="normal">
-              <Input name="phone" placeholder="Phone"/>
+              <Input name="phone" placeholder="Phone" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
           <Col span={12}>
             <Item label="ZIP" offset="normal">
-              <Input name="zip" placeholder="ZIP"/>
+              <Input name="zip" placeholder="ZIP" validate={[Validation.Rules.required]}/>
             </Item>
           </Col>
         </Row>
