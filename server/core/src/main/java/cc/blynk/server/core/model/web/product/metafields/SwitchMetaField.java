fc732830936948bc6d8120e76f0adc7fa99bd257
Dmitriy Dumanskiy <doom369@gmail.com>
2017-06-29 19:37:04 +0300

    #280 additional field value added

--- server/core/src/main/java/cc/blynk/server/core/model/web/product/metafields/SwitchMetaField.java
+++ server/core/src/main/java/cc/blynk/server/core/model/web/product/metafields/SwitchMetaField.java
@@ -12,18 +12,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class SwitchMetaField extends RangeMetaField {
 
+    public final String value;
+
     @JsonCreator
     public SwitchMetaField(@JsonProperty("id") int id,
                            @JsonProperty("name") String name,
                            @JsonProperty("role") Role role,
                            @JsonProperty("from") int from,
-                           @JsonProperty("to") int to) {
+                           @JsonProperty("to") int to,
+                           @JsonProperty("value") String value) {
         super(id, name, role, from, to);
+        this.value = value;
     }
 
     @Override
     public MetaField copy() {
-        return new SwitchMetaField(id, name ,role, from, to);
+        return new SwitchMetaField(id, name ,role, from, to, value);
     }
 
 }
