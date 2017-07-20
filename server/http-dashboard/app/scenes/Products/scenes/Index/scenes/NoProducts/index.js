bfe22746c1cb1ac9ba787d379013daf71515e380
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-07-20 12:11:21 +0300

    #526 Integrate EmptyList into product Empty List

--- server/http-dashboard/app/scenes/Products/scenes/Index/scenes/NoProducts/index.js
+++ server/http-dashboard/app/scenes/Products/scenes/Index/scenes/NoProducts/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import {Link} from 'react-router';
-import {Button} from 'antd';
+import React  from 'react';
+import {
+  MainList,
+  MainLayout
+}             from 'components';
 import './styles.less';
 
 export default class NoProducts extends React.Component {
   render() {
-    return (<div className="products">
-      <div className="products-no-items">
-        <div className="products-no-items-title">Start by creating your first product</div>
-        <div className="products-no-items-description">Product is a digital model of a physical object. It is used in
-          Blynk platform as a template to be assigned to devices. {/*Lean More @todo add link when have this page*/}
-        </div>
-        <div className="products-no-items-action">
-          <Link to="/products/create">
-            <Button icon="plus" type="primary">Create New Product</Button>
-          </Link>
-        </div>
-      </div>
-    </div>
+    return (
+      <MainLayout.Content className="products-empty">
+        <MainList>
+          <MainList.Empty title="Start by creating your first product"
+                          description="Product is a digital model of a physical object. It is used in Blynk platform as a template to be assigned to devices."
+                          link="/products/create"
+                          btnText="Create New Product"/>
+        </MainList>
+      </MainLayout.Content>
     );
   }
 }
