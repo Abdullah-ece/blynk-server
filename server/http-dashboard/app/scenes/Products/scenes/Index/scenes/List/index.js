af23760f5c923cff7bec9a078a5eabeb40a8eaf5
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-08-07 09:14:42 +0300

    #579 Hide product create button if user is logged in sub org

--- server/http-dashboard/app/scenes/Products/scenes/Index/scenes/List/index.js
+++ server/http-dashboard/app/scenes/Products/scenes/Index/scenes/List/index.js
@@ -1,6 +1,7 @@
 import React        from 'react';
 import {Link}       from 'react-router';
 import {Button}     from 'antd';
+import {connect}    from 'react-redux';
 import {
   MainList,
   MainLayout
@@ -8,22 +9,26 @@ import {
 
 import './styles.less';
 
+@connect((state) => ({
+  canCreateProducts: state.Organization && state.Organization.parentId === -1
+}))
 export default class ProductsList extends React.Component {
 
   static propTypes = {
-    products: React.PropTypes.array
+    products: React.PropTypes.array,
+    canCreateProducts: React.PropTypes.bool,
   };
 
   render() {
     return (
       <MainLayout>
-        <MainLayout.Header title="Products" options={(
+        <MainLayout.Header title="Products" options={this.props.canCreateProducts && (
           <div>
             <Link to="/products/create">
               <Button icon="plus" type="primary">Create New Product</Button>
             </Link>
           </div>
-        )}/>
+        ) || null}/>
         <MainLayout.Content>
           <MainList>
             { this.props.products.map((product, key) => (
