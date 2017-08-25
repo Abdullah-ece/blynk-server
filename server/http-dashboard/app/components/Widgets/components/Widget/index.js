7b90be465d1d2a89185d4660fe4bc448f15108f4
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-08-25 10:38:45 +0200

    #654 Product Dashboard Widget DnD styles

--- server/http-dashboard/app/components/Widgets/components/Widget/index.js
+++ server/http-dashboard/app/components/Widgets/components/Widget/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button} from 'antd';
 import Dotdotdot from 'react-dotdotdot';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import './styles.less';
 
 class Widget extends React.Component {
@@ -10,13 +11,19 @@ class Widget extends React.Component {
     children: PropTypes.oneOfType([
       PropTypes.element,
       PropTypes.array,
-    ])
+    ]),
+    className: PropTypes.string
   };
 
   render() {
 
+    const className = classnames({
+      'widgets--widget': true,
+      [this.props.className]: true
+    });
+
     return (
-      <div {...this.props} className="widgets--widget">
+      <div {...this.props} className={className}>
         <div className="widgets--widget-label">
           <Dotdotdot clamp={1}>Widget is there</Dotdotdot>
           <div className="widgets--widget-tools">
