c72a8384dac65dcd7d11984d5a2a0ca6dd19e6ab
Dmitriy Dumanskiy <doom369@gmail.com>
2017-08-29 17:24:33 +0300

    #673 add pinType field to datastream

--- server/core/src/main/java/cc/blynk/server/core/model/web/product/WebDataStream.java
+++ server/core/src/main/java/cc/blynk/server/core/model/web/product/WebDataStream.java
@@ -1,5 +1,6 @@
 package cc.blynk.server.core.model.web.product;
 
+import cc.blynk.server.core.model.enums.PinType;
 import cc.blynk.server.core.model.web.product.metafields.MeasurementUnit;
 import cc.blynk.server.core.model.widgets.CopyObject;
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -24,23 +25,27 @@ public class WebDataStream implements CopyObject<WebDataStream> {
 
     public final byte pin;
 
+    public final PinType pinType;
+
     @JsonCreator
     public WebDataStream(@JsonProperty("id") int id,
                          @JsonProperty("label") String label,
                          @JsonProperty("units") MeasurementUnit units,
                          @JsonProperty("min") double min,
                          @JsonProperty("max") double max,
-                         @JsonProperty("pin") byte pin) {
+                         @JsonProperty("pin") byte pin,
+                         @JsonProperty("pinType") PinType pinType) {
         this.id = id;
         this.label = label;
         this.units = units;
         this.min = min;
         this.max = max;
         this.pin = pin;
+        this.pinType = pinType;
     }
 
     @Override
     public WebDataStream copy() {
-        return new WebDataStream(id, label, units, min, max, pin);
+        return new WebDataStream(id, label, units, min, max, pin, pinType);
     }
 }
