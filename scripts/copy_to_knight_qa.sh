f1a21b439196a993171ada42fe235258476af74b
Dmitriy Dumanskiy <doom369@gmail.com>
2017-10-10 21:39:59 +0300

    Merge branch 'master' of https://github.com/blynkkk/blynk-server
    # Conflicts:
    #	server/http-api/src/main/java/cc/blynk/server/api/http/logic/ResetPasswordLogic.java

--- scripts/copy_to_knight_qa.sh
+++ scripts/copy_to_knight_qa.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-scp ../server/launcher/target/server-0.28.1-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
+scp ../server/launcher/target/server-0.28.3-SNAPSHOT.jar root@knight-qa.blynk.cc:/root
 scp ../server/core/src/main/resources/create_schema.sql root@104.131.50.204:/tmp
