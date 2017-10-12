e8f4180f8e952ca7595da6d60a090440e9f7d94e
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-10-12 13:02:30 +0200

    #778 Split BarChart for Real and Fake

--- server/http-dashboard/app/components/Widgets/components/BarChartWidget/index.js
+++ server/http-dashboard/app/components/Widgets/components/BarChartWidget/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import BarChartSettings from './settings';
 
 class BarChartWidget extends React.Component {
 
-  render() {
+  static propTypes = {
+    fetchRealData: PropTypes.bool
+  };
+
+  renderFakeData() {
     return (
-      <div>The chart is there</div>
+      <div>The chart is fake</div>
     );
+  }
+
+  renderRealData() {
+    return (
+      <div>The chart is real</div>
+    );
+  }
+
+  render() {
+
+    if(!this.props.fetchRealData)
+      return this.renderFakeData();
+
+    return this.renderRealData();
+
   }
 
 }
