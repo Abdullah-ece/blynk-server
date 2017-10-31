f8f2f14b72a47073d1627546073591070eca2f24
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-10-31 14:49:41 +0200

    #804 fix

--- server/http-dashboard/app/services/Numbers/index.js
+++ server/http-dashboard/app/services/Numbers/index.js
@@ -2,7 +2,7 @@ export const addThousandsSeparatorForNumber = (number = 0) => {
   const splitedNumber = number.toString().split('.');
 
   if(splitedNumber[0].length > 3) {
-    splitedNumber[0] = Number(splitedNumber[0]) / 1000;
+    splitedNumber[0] = (Number(splitedNumber[0]) / 1000).toFixed(3);
     splitedNumber[0] = splitedNumber[0].toString().split('.').join(',');
   }
 
