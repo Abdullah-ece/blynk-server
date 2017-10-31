a14f341f126c00f77ad48e3b599a8dcffc8adb9a
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-10-31 15:35:21 +0200

    #804 reimplement addThousandsSeparatorForNumber function

--- server/http-dashboard/app/services/Numbers/index.js
+++ server/http-dashboard/app/services/Numbers/index.js
@@ -1,10 +1,6 @@
-export const addThousandsSeparatorForNumber = (number = 0) => {
-  const splitedNumber = number.toString().split('.');
+export const addThousandsSeparatorForNumber = (num) =>{
+  if(typeof num === "undefined") throw new Error("Argument is missed");
+  if(isNaN(Number(num))) throw new Error("Argument should be a number");
 
-  if(splitedNumber[0].length > 3) {
-    splitedNumber[0] = (Number(splitedNumber[0]) / 1000).toFixed(3);
-    splitedNumber[0] = splitedNumber[0].toString().split('.').join(',');
-  }
-
-  return splitedNumber.join('.');
+  return Number(num).toLocaleString();
 };
