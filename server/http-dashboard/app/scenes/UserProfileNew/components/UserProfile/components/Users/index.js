fe85d1a5fde33a9f9afac33f3025ffa96e72cec5
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-11-10 16:09:12 +0200

    #565 Refactor. Move all connections to state and action creators from component to parent scene. Insert some methods from OrganizationSettings component.

--- server/http-dashboard/app/scenes/UserProfileNew/components/UserProfile/components/Users/index.js
+++ server/http-dashboard/app/scenes/UserProfileNew/components/UserProfile/components/Users/index.js
@@ -2,27 +2,15 @@ import React, {Component} from 'react';
 import {Section, Item} from '../../../Section';
 import InviteUsersForm from './components/InviteUsersForm';
 import OrganizationUsers from './components/OrganizationUsers';
-import {connect} from 'react-redux';
-import {SubmissionError, reset} from 'redux-form';
-import {bindActionCreators} from 'redux';
-import {
-  OrganizationUsersFetch,
-  OrganizationSendInvite
-} from 'data/Organization/actions';
+import {Modal, message} from 'antd';
+import {SubmissionError} from 'redux-form';
 
-@connect((state) => ({
-  Account: state.Account
-}), (dispatch) => ({
-  OrganizationUsersFetch: bindActionCreators(OrganizationUsersFetch, dispatch),
-  OrganizationSendInvite: bindActionCreators(OrganizationSendInvite, dispatch),
-  ResetForm: bindActionCreators(reset, dispatch)
-}))
 class Users extends Component {
   static propTypes = {
-    OrganizationUsersFetch: React.PropTypes.func,
-    OrganizationSendInvite: React.PropTypes.func,
-    ResetForm: React.PropTypes.func,
-    Account: React.PropTypes.object
+    Account: React.PropTypes.object,
+    onOrganizationUsersFetch: React.PropTypes.func,
+    onOrganizationSendInvite: React.PropTypes.func,
+    onResetForm: React.PropTypes.func,
   };
 
   constructor(props) {
@@ -32,16 +20,16 @@ class Users extends Component {
   }
 
   handleInviteSubmit(values) {
-    return this.props.OrganizationSendInvite({
+    return this.props.onOrganizationSendInvite({
       id: this.props.Account.orgId,
       email: values.email,
       name: values.name,
       role: values.role
     }).then(() => {
-      this.props.OrganizationUsersFetch({
+      this.props.onOrganizationUsersFetch({
         id: this.props.Account.orgId
       });
-      this.props.ResetForm('OrganizationSettingsInviteUsersForm');
+      this.props.onResetForm('OrganizationSettingsInviteUsersForm');
       this.showInviteSuccess();
     }).catch((err) => {
       this.showInviteError(
@@ -50,6 +38,18 @@ class Users extends Component {
       new SubmissionError(err);
     });
   }
+
+  showInviteError(message) {
+    Modal.error({
+      title: 'Ooops!',
+      content: String(message)
+    });
+  }
+
+  showInviteSuccess() {
+    message.success('Invite has been sent to email!');
+  }
+
   render() {
     return(
       <div>
