c4a901282fb8a3a9438d1abc0fd284d6ac1039eb
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-11-10 16:29:49 +0200

    #565 delete old UserProfile scene.

--- server/http-dashboard/app/scenes/UserProfile/index.js
+++
@@ -1,4 +0,0 @@
-export MyAccount from './scenes/MyAccount';
-export OrganizationSettings from './scenes/OrganizationSettings';
-export Title from './components/Title';
-export {Section, Item} from './components/Section';
