dc7cac819b09d5f742969e0097b1c2b92fa2ea5a
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2017-11-10 16:05:40 +0200

    #565 connect to methods and data for Branding, OrganizationSettings and Users components and pass them to the child component.

--- server/http-dashboard/app/scenes/UserProfileNew/index.js
+++ server/http-dashboard/app/scenes/UserProfileNew/index.js
@@ -7,15 +7,29 @@ import {
 } from 'data/UserProfile/actions';
 import {bindActionCreators} from 'redux';
 
+import {reset} from 'redux-form';
+
 import {
   AccountSave,
   updateName as AccountUpdateName,
   AccountResetPassword
 } from 'data/Account/actions';
 
+import {
+  OrganizationBrandingUpdate,
+  OrganizationSave,
+  OrganizationLogoUpdate,
+  OrganizationUpdateName,
+  OrganizationUpdateTimezone,
+  OrganizationSendInvite,
+  OrganizationUsersFetch,
+} from 'data/Organization/actions';
+
+
 @connect((state)=>{
   return {
     Account: state.Account,
+    Organization: state.Organization,
     activeTab: state.UserProfile.activeTab,
   };
 },(dispatch) => {
@@ -24,18 +38,44 @@ import {
     AccountUpdateName: bindActionCreators(AccountUpdateName, dispatch),
     AccountSave: bindActionCreators(AccountSave, dispatch),
     AccountResetPassword: bindActionCreators(AccountResetPassword, dispatch),
+
+    OrganizationBrandingUpdate: bindActionCreators(OrganizationBrandingUpdate, dispatch),
+    OrganizationUpdateName: bindActionCreators(OrganizationUpdateName, dispatch),
+    OrganizationLogoUpdate: bindActionCreators(OrganizationLogoUpdate, dispatch),
+    OrganizationSave: bindActionCreators(OrganizationSave, dispatch),
+    OrganizationUpdateTimezone: bindActionCreators(OrganizationUpdateTimezone, dispatch),
+    OrganizationSendInvite: bindActionCreators(OrganizationSendInvite, dispatch),
+    OrganizationUsersFetch: bindActionCreators(OrganizationUsersFetch, dispatch),
+
+    ResetForm: bindActionCreators(reset, dispatch),
   };
 })
 class UserProfile extends Component {
 
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
+
   static propTypes = {
     Account: React.PropTypes.object,
+    Organization: React.PropTypes.object,
     params: React.PropTypes.object,
     activeTab: React.PropTypes.string,
     onTabChange: React.PropTypes.func,
+    ResetForm: React.PropTypes.func,
+
     AccountUpdateName: React.PropTypes.func,
     AccountSave: React.PropTypes.func,
     AccountResetPassword: React.PropTypes.func,
+
+    OrganizationBrandingUpdate: React.PropTypes.func,
+    OrganizationUpdateName: React.PropTypes.func,
+    OrganizationLogoUpdate: React.PropTypes.func,
+    OrganizationSave: React.PropTypes.func,
+    OrganizationUpdateTimezone: React.PropTypes.func,
+    OrganizationUsersFetch: React.PropTypes.func,
+    OrganizationSendInvite: React.PropTypes.func,
+
   };
 
   constructor(props) {
@@ -45,6 +85,13 @@ class UserProfile extends Component {
     this.handleAccountUpdateName = this.handleAccountUpdateName.bind(this);
     this.handleAccountSave = this.handleAccountSave.bind(this);
     this.handleAccountResetPassword = this.handleAccountResetPassword.bind(this);
+    this.handleOrganizationBrandingUpdate = this.handleOrganizationBrandingUpdate.bind(this);
+    this.handleOrganizationUpdateName = this.handleOrganizationUpdateName.bind(this);
+    this.handleOrganizationLogoUpdate = this.handleOrganizationLogoUpdate.bind(this);
+    this.handleOrganizationSave = this.handleOrganizationSave.bind(this);
+    this.handleOrganizationUpdateTimezone = this.handleOrganizationUpdateTimezone.bind(this);
+    this.handleOrganizationUsersFetch = this.handleOrganizationUsersFetch.bind(this);
+    this.handleOrganizationSendInvite = this.handleOrganizationSendInvite.bind(this);
   }
 
   componentWillMount() {
@@ -55,6 +102,7 @@ class UserProfile extends Component {
   }
   handleTabChange(tab) {
     this.props.onTabChange(tab);
+    this.context.router.push(`/UserProfile/${tab}`);
   }
 
   handleAccountUpdateName(name) {
@@ -69,6 +117,35 @@ class UserProfile extends Component {
     return this.props.AccountResetPassword(data);
   }
 
+  handleOrganizationBrandingUpdate(colours) {
+    this.props.OrganizationBrandingUpdate(colours);
+  }
+
+  handleOrganizationLogoUpdate(logo) {
+    this.props.OrganizationLogoUpdate(logo);
+  }
+
+  handleOrganizationSave(data) {
+    return this.props.OrganizationSave(data);
+  }
+
+  handleOrganizationUpdateName(name) {
+    this.props.OrganizationUpdateName(name);
+  }
+
+  handleOrganizationUpdateTimezone(timezone) {
+    this.props.OrganizationUpdateTimezone(timezone);
+  }
+
+  handleOrganizationUsersFetch(data) {
+    this.props.OrganizationUsersFetch(data);
+  }
+
+  handleOrganizationSendInvite(data) {
+    return this.props.OrganizationSendInvite(data);
+  }
+
+
   render() {
     const params = {
       activeTab: this.props.activeTab || TABS.ACCOUNT_SETTINGS.key
@@ -77,10 +154,21 @@ class UserProfile extends Component {
     return(
       <UserProfileComponent params={params}
                             onTabChange={this.handleTabChange}
+                            Account={this.props.Account}
                             onAccountNameUpdate={this.handleAccountUpdateName}
                             onAccountSave={this.handleAccountSave}
                             onAccountResetPassword={this.handleAccountResetPassword}
-                            Account={this.props.Account}/>
+
+                            Organization={this.props.Organization}
+                            onOrganizationSave={this.handleOrganizationSave}
+                            onOrganizationLogoUpdate={this.handleOrganizationLogoUpdate}
+                            onOrganizationUpdateName={this.handleOrganizationUpdateName}
+                            onOrganizationBrandingUpdate={this.handleOrganizationBrandingUpdate}
+                            onOrganizationUpdateTimezone={this.handleOrganizationUpdateTimezone}
+                            onOrganizationUsersFetch = {this.handleOrganizationUsersFetch}
+                            onOrganizationSendInvite = {this.handleOrganizationSendInvite}
+
+                            onResetForm = {this.props.ResetForm}/>
     );
   }
 }
