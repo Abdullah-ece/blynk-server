ba6330ff0131198084f939201c50724362b4914e
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-12-13 14:08:12 +0100

    #913 Ability to edit Label

--- server/http-dashboard/app/components/Widgets/components/LabelWidget/settings.js
+++ server/http-dashboard/app/components/Widgets/components/LabelWidget/settings.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import WidgetSettings from '../WidgetSettings';
 import PropTypes from 'prop-types';
-import {reduxForm, getFormValues, reset, initialize, destroy, change} from 'redux-form';
+import {reduxForm, getFormValues, reset, initialize, destroy, change, Field} from 'redux-form';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fromJS} from 'immutable';
+import {
+  SimpleContentEditable
+} from 'components';
 
 @connect((state, ownProps) => ({
   formValues: fromJS(getFormValues(ownProps.form)(state) || {}),
@@ -36,6 +39,15 @@ class LabelWidgetSettings extends React.Component {
     this.handleCancel = this.handleCancel.bind(this);
   }
 
+  labelNameComponent({input}) {
+    return (
+      <SimpleContentEditable maxLength={35}
+                             className="modal-window-widget-settings-config-widget-name"
+                             value={input.value} o
+                             onChange={input.onChange}/>
+    );
+  }
+
   handleCancel() {
     if (typeof this.props.onClose === 'function')
       this.props.onClose();
@@ -58,7 +70,16 @@ class LabelWidgetSettings extends React.Component {
           <div>Label Widget Preview</div>
         )}
         config={(
-          <div>Label Widget Settings</div>
+          <div>
+            <div className="modal-window-widget-settings-config-column-header">
+              <Field name="label" component={this.labelNameComponent}/>
+
+              <div className="modal-window-widget-settings-config-add-source">
+                {/*<Button type="dashed" onClick={this.handleAddSource}>Add source</Button>*/}
+              </div>
+
+            </div>
+          </div>
         )}/>
     );
   }
