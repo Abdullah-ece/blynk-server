d5c0b882c129bb96f289fbe1267331ab09b00c6b
Ihor Brazhnichenko <ihor.bra@gmail.com>
2017-12-14 14:43:01 +0100

    #913 Ability to select data stream

--- server/http-dashboard/app/components/Widgets/components/LabelWidget/settings.js
+++ server/http-dashboard/app/components/Widgets/components/LabelWidget/settings.js
@@ -6,13 +6,26 @@ import {reduxForm, getFormValues, reset, initialize, destroy, change, Field} fro
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fromJS} from 'immutable';
+import _ from 'lodash';
+import {WIDGETS_SOURCE_TYPES_LIST} from 'services/Widgets';
+import Validation from 'services/Validation';
+import {
+  MetadataSelect as Select
+} from 'components/Form';
+import {
+  Select as AntdSelect
+} from 'antd';
 import {
   SimpleContentEditable
 } from 'components';
+import {
+  Item,
+  ItemsGroup
+} from "components/UI";
 
 @connect((state, ownProps) => ({
-  formValues: fromJS(getFormValues(ownProps.form)(state) || {}),
-  dataStreams: fromJS(state.Product.edit.dataStreams.fields || []),
+  formValues: (getFormValues(ownProps.form)(state) || {}),
+  dataStreams: (state.Product.edit.dataStreams && state.Product.edit.dataStreams.fields || []),
 }), (dispatch) => ({
   changeForm: bindActionCreators(change, dispatch),
   resetForm: bindActionCreators(reset, dispatch),
@@ -27,9 +40,32 @@ class LabelWidgetSettings extends React.Component {
 
     onClose: PropTypes.func,
     resetForm: PropTypes.func,
+    changeForm: PropTypes.func,
     handleSubmit: PropTypes.func,
 
     form: PropTypes.string,
+
+    formValues: PropTypes.shape({
+      id: PropTypes.any,
+      dataSource: PropTypes.array, // type|pin|columnType, e.g. dataStream|100|Start Time
+      sources: PropTypes.arrayOf(PropTypes.shape({
+        sourceType: PropTypes.oneOf(['RAW_DATA', 'SUM', 'AVG', 'MED', 'MIN', 'MAX', 'COUNT']),
+        dataStream: PropTypes.shape({
+          id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+          pin: PropTypes.number,
+          name: PropTypes.string
+        }),
+        selectedColumns: PropTypes.array
+      })),
+    }),
+
+    dataStreams: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      values: PropTypes.shape({
+        pin: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+        label: PropTypes.string,
+      })
+    }))
   };
 
   constructor(props) {
@@ -37,6 +73,147 @@ class LabelWidgetSettings extends React.Component {
 
     this.handleSave = this.handleSave.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
+    this.sourceMultipleSelectComponent = this.sourceMultipleSelectComponent.bind(this);
+  }
+
+  sourceMultipleSelectComponent(props) {
+
+    const onChange = (value) => {
+
+      const getStreamByPin = (pin) => {
+        return _.find(props.dataStreams, (stream) => {
+          return parseInt(stream.values.pin) === parseInt(pin);
+        });
+      };
+
+      const getColumnByLabel = (label = null, columns = []) => {
+        return _.find(columns, (column) => String(column.label) === String(label));
+      };
+
+      let dataStream = null;
+      let selectedColumns = [];
+
+      value.forEach((source) => {
+
+        let [pin, columnLabel] = source.split('|');
+
+        if(!dataStream) {
+          dataStream = getStreamByPin(pin);
+        }
+
+        if(columnLabel) {
+
+          let column = getColumnByLabel(columnLabel, dataStream.values.tableDescriptor.columns);
+
+          selectedColumns.push({
+            name: column.columnName,
+            label: column.label,
+            type: 'COLUMN',
+          });
+
+        }
+
+      });
+
+      if(!dataStream || !dataStream.values) {
+        props.input.onChange({});
+        props.changeForm(this.props.form, 'sources.0.selectedColumns', []);
+        return null;
+      }
+
+      props.input.onChange(
+        fromJS(dataStream.values).set('tableDescriptor', null).toJS()
+      );
+
+      props.changeForm(this.props.form, 'sources.0.selectedColumns', selectedColumns);
+
+    };
+
+    const getValue = () => {
+
+      if(!props.input.value) return [];
+
+      let values = [];
+
+      let pin = this.props.formValues.sources[0].dataStream.pin;
+
+      if(isNaN(Number(pin)))
+        return [];
+
+      (this.props.formValues.sources[0].selectedColumns || []).forEach((column) => {
+        values.push(`${pin}|${column.label}`);
+      });
+
+      if(!values.length) // display stream as selected only if no columns selected
+        values.push(String(pin));
+
+      return values;
+    };
+
+    return this.multipleTagsSelect({
+      ...props,
+      input: {
+        ...props.input,
+        value: getValue(),
+        onChange: onChange,
+        onBlur: () => {},
+      }
+    });
+  }
+
+  multipleTagsSelect(props) {
+
+    const getOption = (item) => {
+      return (
+        <AntdSelect.Option value={item.key} key={item.key} disabled={item.disabled || false}>
+          {item.value}
+        </AntdSelect.Option>
+      );
+    };
+
+    const getOptions = (list) => {
+      return list.map((item) => getOption(item));
+    };
+
+    const getGroup = (name, list) => {
+      return (
+        <AntdSelect.OptGroup key={name}>
+          {getOptions(list)}
+        </AntdSelect.OptGroup>
+      );
+    };
+
+    const getGroups = (list) => {
+      return Object.keys(list).map((key) => getGroup(key, list[key]));
+    };
+
+    const values = props.values || [];
+
+    let optionsList = null;
+
+    if (Array.isArray(values)) {
+      optionsList = getOptions(values);
+    } else {
+      optionsList = getGroups(values);
+    }
+
+    return (
+      <AntdSelect mode="multiple"
+                  onFocus={props.input.onFocus}
+                  onBlur={props.input.onBlur}
+                  onChange={props.input.onChange}
+                  notFoundContent={props.notFoundContent || ''}
+                  dropdownMatchSelectWidth={props.dropdownMatchSelectWidth || false}
+                  value={props.input.value || []}
+                  placeholder={props.placeholder || ''}
+                  validate={props.validate || []}
+                  filterOption={props.filterOption || undefined}
+                  style={{width: '100%'}}
+                  allowClear={true}>
+        {optionsList || null}
+      </AntdSelect>
+    );
+
   }
 
   labelNameComponent({input}) {
@@ -60,7 +237,41 @@ class LabelWidgetSettings extends React.Component {
       this.props.handleSubmit();
   }
 
+  getSourceOptions() {
+    const dataStreamsOptions = [];
+
+    this.props.dataStreams.forEach((stream) => {
+
+      dataStreamsOptions.push({
+        key: `${stream.values.pin}`,
+        value: stream.values.label,
+      });
+
+
+      /* Uncomment when need tableDescription fields listed on Dropdown */
+
+      // if (stream.values.tableDescriptor && stream.values.tableDescriptor.columns) {
+      //   stream.values.tableDescriptor.columns.forEach((column) => {
+      //     dataStreamsOptions.push({
+      //       key: `${stream.values.pin}|${column.label}`,
+      //       value: `-- ${column.label}`,
+      //     });
+      //   });
+      // }
+
+      /* END */
+
+    });
+
+    return {
+      'Data Streams': dataStreamsOptions
+    };
+  }
+
   render() {
+
+    const sourcesOptions = this.getSourceOptions();
+
     return (
       <WidgetSettings
         onSave={this.handleSave}
@@ -78,6 +289,39 @@ class LabelWidgetSettings extends React.Component {
                 {/*<Button type="dashed" onClick={this.handleAddSource}>Add source</Button>*/}
               </div>
 
+              <div className="modal-window-widget-settings-config-column-bar-configuration">
+                <ItemsGroup>
+                  <Item label="X: Data" offset="large">
+                    <Select displayError={false}
+                            dropdownMatchSelectWidth={false}
+                            name="sources.0.sourceType"
+                            values={WIDGETS_SOURCE_TYPES_LIST}
+                            placeholder="Choose type"
+                            filterOption={this.simpleMatch}
+                            validate={[Validation.Rules.required]}
+                            style={{width: '100px'}}/>
+                  </Item>
+                  <Item label=" " offset="large" style={{width: '100%'}}>
+
+                    <Field name="sources.0.dataStream"
+                           style={{width: '100%'}}
+                           placeholder="Choose Source"
+                           validate={[Validation.Rules.required]}
+                           component={this.sourceMultipleSelectComponent}
+                           values={sourcesOptions}
+                           dataStreams={this.props.dataStreams}
+                           formValues={this.props.formValues}
+                           changeForm={this.props.changeForm}
+                           form={this.props.form}
+                           filterOption={this.simpleMatch}
+                           notFoundContent={sourcesOptions["Data Streams"].length > 0 ?
+                             "No DataStreams or Fields match search" : "Create at least one DataStream with PIN >= 100"}
+                    />
+
+                  </Item>
+                </ItemsGroup>
+
+              </div>
             </div>
           </div>
         )}/>
--- server/http-dashboard/app/services/Widgets/index.js
+++ server/http-dashboard/app/services/Widgets/index.js
@@ -211,9 +211,36 @@ export const BAR_CHART_PARAMS = {
 
 };
 
+export const WIDGETS_CHART_TYPES = {
+  'LINE': 'LINE',
+  'DOTS': 'DOTS',
+};
+
+export const WIDGETS_CHART_TYPES_LIST = [
+  {
+    key: WIDGETS_CHART_TYPES.LINE,
+    value: 'Line'
+  },
+  {
+    key: WIDGETS_CHART_TYPES.DOTS,
+    value: 'Dots'
+  }
+];
+
+export const WIDGETS_SOURCE_TYPES = {
+  'RAW_DATA': 'RAW_DATA',
+  'SUM': 'SUM',
+  'AVG': 'AVG',
+  'MED': 'MED',
+  'MIN': 'MIN',
+  'MAX': 'MAX',
+  'COUNT': 'COUNT',
+  'CUMULATIVE_COUNT': 'CUMULATIVE_COUNT',
+};
+
 export const WIDGETS_CONFIGS = {
   [WIDGET_TYPES.LABEL]: {
-
+    'sourceType': WIDGETS_SOURCE_TYPES.RAW_DATA
   },
   [WIDGET_TYPES.LINEAR]: {
     minW: 3,
@@ -246,33 +273,6 @@ export const WIDGETS_CONFIGS = {
     'limit' : BAR_CHART_PARAMS.MAX_ROWS.defaultKey,
     'color': BAR_CHART_PARAMS.COLOR.defaultKey,
   },
-};
-
-export const WIDGETS_CHART_TYPES = {
-  'LINE': 'LINE',
-  'DOTS': 'DOTS',
-};
-
-export const WIDGETS_CHART_TYPES_LIST = [
-  {
-    key: WIDGETS_CHART_TYPES.LINE,
-    value: 'Line'
-  },
-  {
-    key: WIDGETS_CHART_TYPES.DOTS,
-    value: 'Dots'
-  }
-];
-
-export const WIDGETS_SOURCE_TYPES = {
-  'RAW_DATA': 'RAW_DATA',
-  'SUM': 'SUM',
-  'AVG': 'AVG',
-  'MED': 'MED',
-  'MIN': 'MIN',
-  'MAX': 'MAX',
-  'COUNT': 'COUNT',
-  'CUMULATIVE_COUNT': 'CUMULATIVE_COUNT',
 };
 
 export const WIDGETS_SOURCE_TYPES_LIST = [
@@ -363,6 +363,12 @@ export const WIDGETS_PREDEFINED_OPTIONS = {
     w: 3,
     h: 2,
     label: "Label",
+    sources: [
+      {
+        id: 1,
+        ...WIDGETS_CONFIGS[WIDGET_TYPES.LABEL]
+      }
+    ]
   }
 };
 
