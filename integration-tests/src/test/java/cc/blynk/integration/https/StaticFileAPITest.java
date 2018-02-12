8ab9779384bd8ab9479c30e09b38ab91ac0c1f84
Dmitriy Dumanskiy <doom369@gmail.com>
2018-02-12 22:41:02 +0200

    additional test

--- integration-tests/src/test/java/cc/blynk/integration/https/StaticFileAPITest.java
+++ integration-tests/src/test/java/cc/blynk/integration/https/StaticFileAPITest.java
@@ -3,11 +3,18 @@ package cc.blynk.integration.https;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.net.ssl.SSLContext;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * The Blynk Project.
@@ -16,6 +23,73 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class StaticFileAPITest extends APIBaseTest {
+
+    @Test
+    public void getRootUrl() throws Exception {
+        String url = String.format("https://localhost:%s", httpsPort);
+
+        HttpGet index = new HttpGet(url);
+
+        String cssUrl;
+        String jsUrl;
+
+        try (CloseableHttpResponse response = httpclient.execute(index)) {
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            String indexPageString = consumeText(response);
+            assertNotNull(indexPageString);
+            assertTrue(indexPageString.contains("<title>Knight</title>"));
+
+            cssUrl = indexPageString.substring(
+                    indexPageString.indexOf("/static/main."),
+                    indexPageString.indexOf(".css") + 4
+            );
+
+            jsUrl = indexPageString.substring(
+                    indexPageString.indexOf("/static/main.", indexPageString.indexOf("/static/main.") + 1),
+                    indexPageString.indexOf(".js") + 3
+            );
+        }
+
+        SSLContext sslcontext = initUnsecuredSSLContext();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new MyHostVerifier());
+
+        new Thread(() -> {
+            try {
+                CloseableHttpClient httpclient = HttpClients.custom()
+                        .setSSLSocketFactory(sslsf)
+                        .build();
+
+                HttpGet css = new HttpGet(url + cssUrl);
+                try (CloseableHttpResponse response = httpclient.execute(css)) {
+                    assertEquals(200, response.getStatusLine().getStatusCode());
+                    String indexPageString = consumeText(response);
+                    assertNotNull(indexPageString);
+                }
+            } catch (Exception e) {
+
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            try {
+                CloseableHttpClient httpclient = HttpClients.custom()
+                        .setSSLSocketFactory(sslsf)
+                        .build();
+
+                HttpGet js = new HttpGet(url + jsUrl);
+                try (CloseableHttpResponse response = httpclient.execute(js)) {
+                    assertEquals(200, response.getStatusLine().getStatusCode());
+                    String indexPageString = consumeText(response);
+                    assertNotNull(indexPageString);
+                    assertTrue(Integer.valueOf(response.getFirstHeader("Content-Length").getValue()) > 3_000_000);
+                }
+            } catch (Exception e) {
+
+            }
+        }).start();
+        sleep(1000);
+    }
 
     @Test
     public void getIndexPage() throws Exception {
