802abb990d231b0b25b03e6b88c565e712e51455
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2018-02-23 15:48:20 +0200

    #1008 Add "back to login" button after password reset sent

--- server/http-dashboard/app/scenes/ForgotPass/components/Confirmation.js
+++ server/http-dashboard/app/scenes/ForgotPass/components/Confirmation.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import {Button} from 'antd';
+import PropTypes from 'prop-types';
 
 export default class Confirmation extends React.Component {
 
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.backButtonHandler = this.backButtonHandler.bind(this);
+  }
+
+  backButtonHandler() {
+    this.context.router.push('/login');
+  }
+
   render() {
-    return (<div className="confirm-container">
-      <div className="form-header">Forgot password?</div>
-      <div className="confirm-message">
-        Please check your email for further instructions.
+    return (
+      <div className="confirm-container">
+        <div className="form-header">Password restoration</div>
+        <div className="password-restoration-info-message">
+          Check your inbox for instructions on how to restore password
+        </div>
+        <Button type="primary"
+                size="default"
+                onClick={this.backButtonHandler}>
+                Back to Login
+        </Button>
       </div>
-    </div>);
+    );
   }
 }
