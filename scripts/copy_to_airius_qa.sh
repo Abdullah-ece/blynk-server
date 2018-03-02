735081ccd0031bf34a23d6d7b0d68c000dcf3499
Dmitriy Dumanskiy <doom369@gmail.com>
2018-03-02 18:23:20 +0200

    airius

---
+++ scripts/copy_to_airius_qa.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp ../server/launcher/target/airiusfans-0.33.0-SNAPSHOT.jar root@airiusfans-qa.blynk.cc:/root
+scp ../server/core/src/main/resources/create_schema.sql root@104.131.50.204:/tmp
