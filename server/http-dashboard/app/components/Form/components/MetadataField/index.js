999659b971854be917b4ecad8ff542bbd97b99fa
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2018-03-13 16:33:49 +0200

    #1135 Add maxLength property to number metadata

--- server/http-dashboard/app/components/Form/components/MetadataField/index.js
+++ server/http-dashboard/app/components/Form/components/MetadataField/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import {Form, Input, Icon} from 'antd';
 
 import {Field as FormField} from 'redux-form';
 
 export default class Field extends React.Component {
+
+  static propTypes = {
+    maxLength: PropTypes.number
+  };
+
+  constructor(props) {
+    super(props);
+    this.renderField = this.renderField.bind(this);
+  }
+
   renderField({validateOnBlur = false, className, disabled = false, displayError = true, style, placeholder, input, type, icon, meta: {active, touched, error, warning}}) {
 
     let validateStatus = 'success';
@@ -29,7 +39,7 @@ export default class Field extends React.Component {
                  help={help}
                  style={style}
                  className={className}>
-        <Input {...input} type={type} placeholder={placeholder} disabled={disabled}
+        <Input maxLength={this.props.maxLength || false} {...input} type={type} placeholder={placeholder} disabled={disabled}
                prefix={icon ? <Icon type={icon} className="form--field-icon"/> : null}/>
       </Form.Item>
     );
