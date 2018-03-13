5fa838dcbcf1ae65e46c8b1ce47917896768f049
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2018-03-13 16:45:04 +0200

    #1134 Remove unit select in Unit metadata on device detail page

--- server/http-dashboard/app/components/DeviceMetadata/components/Unit/modal.js
+++ server/http-dashboard/app/components/DeviceMetadata/components/Unit/modal.js
@@ -1,69 +1,13 @@
 import React from 'react';
 import {ItemsGroup, Item, Input} from 'components/UI';
-import {MetadataSelect} from 'components/Form';
 import Validation from 'services/Validation';
-import {Unit} from 'services/Products';
 
 class UnitModal extends React.Component {
-
-  Unit = {
-    'Length, Distance': {
-      'Imperial': [
-        Unit.Inch,
-        Unit.Foot,
-        Unit.Yard,
-        Unit.Mile
-      ],
-      'Metric': [
-        Unit.Millimeter,
-        Unit.Centimeter,
-        Unit.Meter,
-        Unit.Kilometer
-      ]
-    },
-    'Mass': {
-      'Imperial': [
-        Unit.Ounce,
-        Unit.Pound,
-        Unit.Stone,
-        Unit.Quarter,
-        Unit.Hundredweight,
-        Unit.Ton
-      ],
-      'Metric': [
-        Unit.Milligram,
-        Unit.Gram,
-        Unit.Kilogram
-      ]
-    },
-    'Volume': {
-      'Imperial': [
-        Unit.Pint,
-        Unit.Gallon
-      ],
-      'Metric': [
-        Unit.Liter
-      ]
-    },
-    'Temperature': [
-      Unit.Celsius,
-      Unit.Fahrenheit,
-      Unit.Kelvin
-    ],
-    'Other': [
-      Unit.Percentage
-    ]
-  };
 
   render() {
     return (
       <div>
         <ItemsGroup>
-          <Item label="Units">
-            <MetadataSelect name="units" type="text" placeholder="Units"
-                            dropdownClassName="product-metadata-item-unit-dropdown" values={this.Unit}
-                            style={{width: '100%'}}/>
-          </Item>
           <Item label="Value">
             <Input name="value" type="text" placeholder="Value" validate={[
               Validation.Rules.number,
