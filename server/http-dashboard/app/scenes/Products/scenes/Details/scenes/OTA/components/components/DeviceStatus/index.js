8c281e9215a93b69d5cd018faa60f291f276840f
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2018-05-07 14:22:31 +0300

    #1267 Implement device status for OTA table

---
+++ server/http-dashboard/app/scenes/Products/scenes/Details/scenes/OTA/components/components/DeviceStatus/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Popover} from 'antd';
+import {convertTimeStampToTime} from 'services/Time';
+
+import './styles.less';
+
+class DeviceStatus extends React.Component {
+
+  static propTypes = {
+    status: PropTypes.string,
+    disconnectTime: PropTypes.number,
+  };
+
+  render(){
+
+    return(
+      <div>
+        {this.props.status === "ONLINE" && (<div className={"devices-list-item-status-online"} />) ||
+          (
+            <Popover content={
+              this.props.disconnectTime === 0 && (<div>wasn't online yet</div>) || (<div>{"Offline for "+ convertTimeStampToTime(Date.now() - this.props.disconnectTime)}</div>) } >
+              <div className={"devices-list-item-status-offline"} />
+            </Popover>
+          )}
+      </div>
+    );
+  }
+}
+
+export default DeviceStatus;
