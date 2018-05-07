b4c094c51bf70462c8f6b9b11696d98b533ef684
Ihor Brazhnichenko <ihor.bra@gmail.com>
2018-05-07 12:15:42 +0300

    #1267 Add static data for component

--- server/http-dashboard/app/scenes/Products/scenes/Details/scenes/OTA/index.js
+++ server/http-dashboard/app/scenes/Products/scenes/Details/scenes/OTA/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import OTA from './components';
+import PropTypes from 'prop-types';
 
 class OTAScene extends React.Component{
 
+  static propTypes = {
+    devices: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      status: PropTypes.oneOf(['ONLINE', 'OFFLINE']), // use this for column "status" and display like a green / gray dot
+      disconnectTime: PropTypes.number, // display "Was online N days ago" when user do mouseover the gray dot (idea is to display last time when device was online if it's offline right now)
+      hardwareInfo: PropTypes.shape({
+        version: PropTypes.string
+      })
+    }))
+  };
+
   render(){
-     return (<OTA/>);
+
+    const devices = [{
+      id: 1,
+      name: 'Device 1',
+      status: 'ONLINE',
+      disconnectTime: 1525684476846,
+      hardwareInfo: {
+        version: '0.0.1'
+      }
+    },{
+      id: 2,
+      name: 'Device 2',
+      status: 'OFFLINE',
+      disconnectTime: 1525684476846,
+      hardwareInfo: {
+        version: '0.0.2'
+      }
+    }];
+
+     return (
+       <OTA devices={devices}/>
+     );
   }
 }
 
