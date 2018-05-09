06672a084839dd04803cfb6f3775e3c44511990a
Ihor Brazhnichenko <ihor.bra@gmail.com>
2018-05-09 12:29:50 +0300

    #1267 Fix timeline text

--- server/http-dashboard/app/services/Time/index.js
+++ server/http-dashboard/app/services/Time/index.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 export const convertTimeStampToTime = (timestamp) => {
   if(timestamp / 1000 / 60 > 1){
-    return moment.duration(timestamp / 1000, "seconds").format("d[d] h[h] m[min]");
+    return moment.duration(timestamp / 1000, "seconds").format("d [d] h [h] m [min]");
   } else {
     return Math.floor(timestamp / 1000) + " seconds";
   }
