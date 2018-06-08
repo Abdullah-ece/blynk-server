525239c8380c129691a6593a3624038972cc5725
Oleg Brazhnichenko <oleg.brazhnichenko@gmail.com>
2018-06-08 12:38:35 +0300

    #629 Change data format

--- server/http-dashboard/app/services/Date/index.js
+++ server/http-dashboard/app/services/Date/index.js
@@ -4,13 +4,13 @@ const dataFormatsForCountries = {
   USA: ["MM","DD","YYYY"]
 };
 
-const getTimeConfig = (dateFormat) => {
+const getTimeConfig = (/*dateFormat*/) => {
 
   return {
-    sameDay: '[Today], hh:mm A',
+    sameDay: 'hh:mm A, [Today]',
     lastDay: '[Yesterday], hh:mm A',
-    lastWeek: 'dddd, hh:mm A',
-    sameElse: 'hh:mm A, ' + dateFormat
+    lastWeek: 'hh:mm A, MMM DD.YYYY',
+    sameElse: 'hh:mm A, MMM DD.YYYY'
   };
 };
 
@@ -23,7 +23,7 @@ export const getFormatedDate = (date = Date.now(), country = "USA", separator = 
   return moment(Number(date)).format(getDateFormat(country, separator));
 };
 
-export const getCalendarFormatDate = (time = Date.now(), country = "USA", separator = '.') => {
+export const getCalendarFormatDate = (time = Date.now(), /*country = "USA", separator = '.'*/) => {
 
-  return (moment(Number(time)).calendar(null, getTimeConfig(getDateFormat(country, separator))));
+  return (moment(Number(time)).calendar(null, getTimeConfig(/*getDateFormat(country, separator)*/)));
 };
