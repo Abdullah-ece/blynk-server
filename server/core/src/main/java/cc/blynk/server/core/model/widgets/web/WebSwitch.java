7ea02c5ebda9ec8bd4a095ea54c56685ea4eb3f2
Dmitriy Dumanskiy <doom369@gmail.com>
2018-08-30 13:29:16 +0300

    #1090 correct equals method for the WebSwitch

--- server/core/src/main/java/cc/blynk/server/core/model/widgets/web/WebSwitch.java
+++ server/core/src/main/java/cc/blynk/server/core/model/widgets/web/WebSwitch.java
@@ -3,6 +3,8 @@ package cc.blynk.server.core.model.widgets.web;
 import cc.blynk.server.core.model.enums.PinMode;
 import cc.blynk.server.core.model.widgets.outputs.TextAlignment;
 
+import java.util.Objects;
+
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
@@ -43,50 +45,28 @@ public class WebSwitch extends WebWidget {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof WebSwitch)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         if (!super.equals(o)) {
             return false;
         }
-
         WebSwitch webSwitch = (WebSwitch) o;
-
-        if (isSwitchLabelsEnabled != webSwitch.isSwitchLabelsEnabled) {
-            return false;
-        }
-        if (isWidgetNameHidden != webSwitch.isWidgetNameHidden) {
-            return false;
-        }
-        if (onValue != null ? !onValue.equals(webSwitch.onValue) : webSwitch.onValue != null) {
-            return false;
-        }
-        if (offValue != null ? !offValue.equals(webSwitch.offValue) : webSwitch.offValue != null) {
-            return false;
-        }
-        if (onLabel != null ? !onLabel.equals(webSwitch.onLabel) : webSwitch.onLabel != null) {
-            return false;
-        }
-        if (offLabel != null ? !offLabel.equals(webSwitch.offLabel) : webSwitch.offLabel != null) {
-            return false;
-        }
-        if (alignment != webSwitch.alignment) {
-            return false;
-        }
-        return labelPosition == webSwitch.labelPosition;
+        return isSwitchLabelsEnabled == webSwitch.isSwitchLabelsEnabled
+                && isWidgetNameHidden == webSwitch.isWidgetNameHidden
+                && Objects.equals(onValue, webSwitch.onValue)
+                && Objects.equals(offValue, webSwitch.offValue)
+                && Objects.equals(onLabel, webSwitch.onLabel)
+                && Objects.equals(offLabel, webSwitch.offLabel)
+                && alignment == webSwitch.alignment
+                && labelPosition == webSwitch.labelPosition
+                && Objects.equals(color, webSwitch.color);
     }
 
     @Override
     public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (onValue != null ? onValue.hashCode() : 0);
-        result = 31 * result + (offValue != null ? offValue.hashCode() : 0);
-        result = 31 * result + (onLabel != null ? onLabel.hashCode() : 0);
-        result = 31 * result + (offLabel != null ? offLabel.hashCode() : 0);
-        result = 31 * result + (alignment != null ? alignment.hashCode() : 0);
-        result = 31 * result + (labelPosition != null ? labelPosition.hashCode() : 0);
-        result = 31 * result + (isSwitchLabelsEnabled ? 1 : 0);
-        result = 31 * result + (isWidgetNameHidden ? 1 : 0);
-        return result;
+        return Objects.hash(super.hashCode(), onValue, offValue, onLabel,
+                offLabel, alignment, labelPosition,
+                isSwitchLabelsEnabled, isWidgetNameHidden, color);
     }
 }
