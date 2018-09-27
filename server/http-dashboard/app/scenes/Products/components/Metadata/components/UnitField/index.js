b91fcb4244d7cbb9d64fe15a1c125d67be8aad37
Ihor Brazhnichenko <ihor.bra@gmail.com>
2018-09-27 22:52:39 +0200

    #1396 Update Unit Metafield Layout

--- server/http-dashboard/app/scenes/Products/components/Metadata/components/UnitField/index.js
+++ server/http-dashboard/app/scenes/Products/components/Metadata/components/UnitField/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormItem from 'components/FormItem';
-import {Input} from 'antd';
-import {MetadataField as MetadataFormField, MetadataSelect as MetadataFormSelect} from 'components/Form';
+import {Input, Row, Col} from 'antd';
+import {MetadataField as MetadataFormField, MetadataSelect as MetadataFormSelect, Field} from 'components/Form';
 import Validation from 'services/Validation';
 import BaseField from '../BaseField';
 import {Unit} from 'services/Products';
@@ -26,14 +26,14 @@ class UnitField extends BaseField {
         Unit.Mile,
         Unit.SquareFeet,
       ],
-      'Metric': [
+      'Metric'  : [
         Unit.Millimeter,
         Unit.Centimeter,
         Unit.Meter,
         Unit.Kilometer
       ]
     },
-    'Duration':[
+    'Duration'        : [
       Unit.Year,
       Unit.Month,
       Unit.Week,
@@ -42,7 +42,7 @@ class UnitField extends BaseField {
       Unit.Minute,
       Unit.Second
     ],
-    'Mass': {
+    'Mass'            : {
       'Imperial': [
         Unit.Ounce,
         Unit.Pound,
@@ -52,27 +52,27 @@ class UnitField extends BaseField {
         Unit.Ton,
         Unit.Tonne,
       ],
-      'Metric': [
+      'Metric'  : [
         Unit.Milligram,
         Unit.Gram,
         Unit.Kilogram
       ]
     },
-    'Volume': {
+    'Volume'          : {
       'Imperial': [
         Unit.Pint,
         Unit.Gallon
       ],
-      'Metric': [
+      'Metric'  : [
         Unit.Liter
       ]
     },
-    'Temperature': [
+    'Temperature'     : [
       Unit.Celsius,
       Unit.Fahrenheit,
       Unit.Kelvin
     ],
-    'Other': [
+    'Other'           : [
       Unit.Percentage,
       Unit.PercentageSymbol,
     ]
@@ -84,7 +84,7 @@ class UnitField extends BaseField {
     const units = this.props.field.get('units');
 
     return {
-      name: name && typeof name === 'string' ? `${name.trim()}` : null,
+      name : name && typeof name === 'string' ? `${name.trim()}` : null,
       value: value && typeof value === 'string' && units ? `${value} ${Unit[units].abbreviation}` : null
     };
   }
@@ -93,48 +93,72 @@ class UnitField extends BaseField {
 
     return (
       <div>
-        <FormItem offset={false}>
-          <FormItem.TitleGroup>
-            <FormItem.Title style={{width: '50%'}}>Measurement</FormItem.Title>
-            <FormItem.Title style={{width: '25%'}}>Units</FormItem.Title>
-            <FormItem.Title style={{width: '25%'}}>Value</FormItem.Title>
-          </FormItem.TitleGroup>
-          <FormItem.Content>
-            <Input.Group compact>
-              <MetadataFormField className={`metadata-name-field-${this.props.field.get('id')}`}
-                                 onFocus={this.onFocus} onBlur={this.onBlur}
-                                 validateOnBlur={true} name={`metaFields.${this.props.metaFieldKey}.name`} type="text" placeholder="Field Name"
-                                 style={{width: '50%'}} validate={[
-                Validation.Rules.required, Validation.Rules.metafieldName,
-              ]}/>
-              <MetadataFormSelect style={{width: '25%'}} onFocus={this.onFocus} onBlur={this.onBlur}
-                                  name={`metaFields.${this.props.metaFieldKey}.units`} type="text" placeholder="Choose"
-                                  dropdownClassName="product-metadata-item-unit-dropdown" values={this.Unit}/>
-              <MetadataFormField style={{width: '25%'}} onFocus={this.onFocus} onBlur={this.onBlur}
-                                 name={`metaFields.${this.props.metaFieldKey}.value`} type="text" placeholder="Default val..." validate={[
-                Validation.Rules.number
-              ]}/>
-            </Input.Group>
-          </FormItem.Content>
-        </FormItem>
-        <FormItem offset={false}>
-          <FormItem.TitleGroup>
-            <FormItem.Title style={{width: '50%'}}>Min/Max values (optional)</FormItem.Title>
-          </FormItem.TitleGroup>
-          <FormItem.Content>
-            <Input.Group compact>
+        <Field title="Number"
+               className={`metadata-name-field-${this.props.field.get('id')} normal-offset`}
+               onFocus={this.onFocus}
+               onBlur={this.onBlur}
+               validateOnBlur={true}
+               name={`metaFields.${this.props.metaFieldKey}.name`}
+               placeholder="Field Name"
+               validate={[Validation.Rules.required]}
+        />
 
-              <MetadataFormField style={{width: '20%'}} onFocus={this.onFocus} onBlur={this.onBlur}
-                                 name={`metaFields.${this.props.metaFieldKey}.min`} type="text" placeholder="Min" validate={[
-                Validation.Rules.number
-              ]}/>
-              <MetadataFormField style={{width: '20%'}} onFocus={this.onFocus} onBlur={this.onBlur}
-                                 name={`metaFields.${this.props.metaFieldKey}.max`} type="text" placeholder="Max" validate={[
-                Validation.Rules.number
-              ]}/>
-            </Input.Group>
-          </FormItem.Content>
-        </FormItem>
+        <Row>
+          <Col span={7}>
+
+            <FormItem offset={false}>
+              <FormItem.TitleGroup>
+                <FormItem.Title style={{width: '50%'}}>Value</FormItem.Title>
+                <FormItem.Title style={{width: '50%'}}>Units</FormItem.Title>
+              </FormItem.TitleGroup>
+              <FormItem.Content>
+                <Input.Group compact>
+                  <MetadataFormField style={{width: '50%'}} onFocus={this.onFocus} onBlur={this.onBlur}
+                                     name={`metaFields.${this.props.metaFieldKey}.value`} type="text"
+                                     placeholder="Default val..." validate={[
+                    Validation.Rules.number
+                  ]}/>
+                  <MetadataFormSelect style={{width: '50%'}} onFocus={this.onFocus} onBlur={this.onBlur}
+                                      name={`metaFields.${this.props.metaFieldKey}.units`} type="text"
+                                      placeholder="Choose"
+                                      dropdownClassName="product-metadata-item-unit-dropdown" values={this.Unit}/>
+                </Input.Group>
+              </FormItem.Content>
+            </FormItem>
+          </Col>
+          <Col span={9} offset={1}>
+            <FormItem offset={false}>
+              <FormItem.TitleGroup>
+                <FormItem.Title style={{width: '100%'}}>Min/Max values (optional)</FormItem.Title>
+              </FormItem.TitleGroup>
+              <FormItem.Content>
+                <Input.Group compact>
+
+                  <MetadataFormField style={{width: '50%'}} onFocus={this.onFocus} onBlur={this.onBlur}
+                                     name={`metaFields.${this.props.metaFieldKey}.min`} type="text" placeholder="Min" validate={[
+                    Validation.Rules.number
+                  ]}/>
+                  <MetadataFormField style={{width: '50%'}} onFocus={this.onFocus} onBlur={this.onBlur}
+                                     name={`metaFields.${this.props.metaFieldKey}.max`} type="text" placeholder="Max" validate={[
+                    Validation.Rules.number
+                  ]}/>
+                </Input.Group>
+              </FormItem.Content>
+            </FormItem>
+
+          </Col>
+          <Col span={6} offset={1}>
+            <Field title="Step"
+                   className={`metadata-name-field-${this.props.field.get('id')} normal-offset`}
+                   onFocus={this.onFocus}
+                   onBlur={this.onBlur}
+                   validateOnBlur={true}
+                   name={`metaFields.${this.props.metaFieldKey}.step`}
+                   placeholder="Optional"
+            />
+          </Col>
+        </Row>
+
       </div>
     );
   }
