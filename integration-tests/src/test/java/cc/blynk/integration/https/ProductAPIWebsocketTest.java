bad4db668627eae3e730928404cae6b9fb525ca5
Dmitriy Dumanskiy <doom369@gmail.com>
2018-10-05 16:23:09 +0300

    #1520 test moved

--- integration-tests/src/test/java/cc/blynk/integration/https/ProductAPIWebsocketTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/https/ProductAPIWebsocketTest.java
@@ -8,10 +8,19 @@ import cc.blynk.server.core.model.device.ConnectionType;
 import cc.blynk.server.core.model.device.Device;
 import cc.blynk.server.core.model.enums.PinType;
 import cc.blynk.server.core.model.enums.SortOrder;
+import cc.blynk.server.core.model.serialization.JsonParser;
 import cc.blynk.server.core.model.web.Organization;
+import cc.blynk.server.core.model.web.product.EventReceiver;
 import cc.blynk.server.core.model.web.product.MetaField;
+import cc.blynk.server.core.model.web.product.MetadataType;
 import cc.blynk.server.core.model.web.product.Product;
 import cc.blynk.server.core.model.web.product.WebDashboard;
+import cc.blynk.server.core.model.web.product.events.CriticalEvent;
+import cc.blynk.server.core.model.web.product.events.Event;
+import cc.blynk.server.core.model.web.product.events.InformationEvent;
+import cc.blynk.server.core.model.web.product.events.OfflineEvent;
+import cc.blynk.server.core.model.web.product.events.OnlineEvent;
+import cc.blynk.server.core.model.web.product.events.WarningEvent;
 import cc.blynk.server.core.model.web.product.metafields.AddressMetaField;
 import cc.blynk.server.core.model.web.product.metafields.ContactMetaField;
 import cc.blynk.server.core.model.web.product.metafields.CoordinatesMetaField;
@@ -59,6 +68,86 @@ public class ProductAPIWebsocketTest extends SingleServerInstancePerTestWithDBAn
         AppWebSocketClient client = loggedDefaultClient(getUserName(), "1");
         client.getProduct(1333);
         client.verifyResult(webJson(1, "Cannot find product with passed id."));
+    }
+
+    @Test
+    public void printProduct() throws Exception {
+        Product product = new Product();
+        product.id = 1;
+        product.name = "My Product";
+        product.boardType = "Arduino UNO";
+        product.connectionType = ConnectionType.WI_FI;
+        product.description = "Description";
+        product.logoUrl = "/static/logo.png";
+
+        product.metaFields = new MetaField[] {
+                new TextMetaField(1, "My Farm", new int[] {1}, false, false, false, null, "Farm of Smith"),
+                new RangeTimeMetaField(2, "Farm of Smith", new int[] {1}, false, false, false, null, ofSecondOfDay(60),  ofSecondOfDay(120)),
+                new NumberMetaField(3, "Farm of Smith", new int[] {1}, false, false, false, null, 0, 100, 10.222, 1),
+                new MeasurementUnitMetaField(4, "Farm of Smith", new int[] {1}, false, false, false, null, MeasurementUnit.Celsius, 36, 0, 100, 1),
+                new CostMetaField(5, "Farm of Smith", new int[] {1}, false, false, false, null, Currency.getInstance("USD"), 9.99, 1, MeasurementUnit.Gallon, 0, 100),
+                new ContactMetaField(6, "Farm of Smith", new int[] {1}, false, false, false, "Tech Support",
+                        "Dmitriy", false, "Dumanskiy", false, "dmitriy@blynk.cc", false,
+                        "+38063673333",  false, "My street", false, "Ukraine", false,
+                        "Kyiv", false, "Ukraine", false, "03322", false, false),
+                new AddressMetaField(7, "Farm of Smith", new int[] {1}, false, false, false, null, "My street", false,
+                        "San Diego", false, "CA", false, "03322", false, "US", false, false),
+                new CoordinatesMetaField(8, "Farm Location", new int[] {1}, false, false, false, null, 22.222, 23.333),
+                new TimeMetaField(9,"Some Time", new int[] {1}, false, false, false, null, new Date().getTime())
+        };
+
+        product.dataStreams = new DataStream[] {
+                new DataStream(0, (byte) 0, false, false, PinType.VIRTUAL, null, 0, 50, "Temperature", MeasurementUnit.Celsius)
+        };
+
+        EventReceiver eventReceiver = new EventReceiver(1, MetadataType.Contact, "Farm Owner");
+
+        OnlineEvent onlineEvent = new OnlineEvent(
+                1, "Your device is online.", null, false,
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver}
+        );
+
+
+        OfflineEvent offlineEvent = new OfflineEvent(
+                2, "Your device is offline.", null , false,
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                1000
+        );
+
+        InformationEvent infoEvent = new InformationEvent(
+                3, "Door is opened", "Kitchen door is opened.", false, "door_opened",
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver}
+        );
+
+        WarningEvent warningEvent = new WarningEvent(
+                4, "Temperature is high!", "Room temp is high", false, "temp_is_high",
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver}
+        );
+
+        CriticalEvent criticalEvent = new CriticalEvent(
+                5, "Temperature is super high!", "Room temp is super high", false, "temp_is_super_high",
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver},
+                new EventReceiver[]{eventReceiver}
+        );
+
+        product.events = new Event[] {
+                onlineEvent,
+                offlineEvent,
+                infoEvent,
+                warningEvent,
+                criticalEvent
+        };
+
+        System.out.println(JsonParser.MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(product));
     }
 
     @Test
@@ -486,7 +575,7 @@ public class ProductAPIWebsocketTest extends SingleServerInstancePerTestWithDBAn
         textMetaField = (TextMetaField) newDevice.metaFields[0];
         assertEquals(1, textMetaField.id);
         assertEquals("My test metafield", textMetaField.name);
-        assertEquals(1, textMetaField.roleIds);
+        assertEquals(1, textMetaField.roleIds[0]);
         assertEquals("My updated value", textMetaField.value);
 
         fromApiProduct.metaFields = new MetaField[] {
@@ -506,13 +595,13 @@ public class ProductAPIWebsocketTest extends SingleServerInstancePerTestWithDBAn
         textMetaField = (TextMetaField) newDevice.metaFields[0];
         assertEquals(1, textMetaField.id);
         assertEquals("My test metafield", textMetaField.name);
-        assertEquals(1, textMetaField.roleIds);
+        assertEquals(1, textMetaField.roleIds[0]);
         assertEquals("My updated value", textMetaField.value);
 
         NumberMetaField numberMetaField = (NumberMetaField) newDevice.metaFields[1];
         assertEquals(2, numberMetaField.id);
         assertEquals("New metafield", numberMetaField.name);
-        assertEquals(1, numberMetaField.roleIds);
+        assertEquals(1, numberMetaField.roleIds[0]);
         assertEquals(123, numberMetaField.value, 0.1);
     }
 
