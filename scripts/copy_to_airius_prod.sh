98a30f28881f92e719eff702184532dc4c95ee4c
Dmitriy Dumanskiy <doom369@gmail.com>
2018-11-02 12:25:00 +0200

    version up

--- scripts/copy_to_airius_prod.sh
+++ scripts/copy_to_airius_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-scp ../server/launcher/target/airiusfans-0.40.1-SNAPSHOT.jar root@airiusfans.blynk.cc:/root
+scp ../server/launcher/target/airiusfans-0.41.0-SNAPSHOT.jar root@airiusfans.blynk.cc:/root
 scp ../server/core/src/main/resources/create_schema.sql root@104.131.50.204:/tmp
