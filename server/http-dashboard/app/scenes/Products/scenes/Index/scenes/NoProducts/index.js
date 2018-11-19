6fe78ede8651ffe83cb0704cea31d77573a7c105
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-11-19 12:44:28 +0200

    #1649 - updated button text

--- server/http-dashboard/app/scenes/Products/scenes/Index/scenes/NoProducts/index.js
+++ server/http-dashboard/app/scenes/Products/scenes/Index/scenes/NoProducts/index.js
@@ -28,7 +28,7 @@ export default class NoProducts extends React.Component {
           <MainList.Empty title="Start by creating your first product"
                           description="Product is a digital model of a physical object. It is used in Blynk platform as a template to be assigned to devices."
                           link="/products/create"
-                          btnText="Create New Product"/>
+                          btnText="New Product"/>
         </MainList>
       </MainLayout.Content>
     );
