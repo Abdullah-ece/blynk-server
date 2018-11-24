ae6f8ef369d77a783171200c7ae2b57ab4a2f0ab
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-11-24 03:47:49 +0200

    #1728 - added error message display for account udpdate failures

--- server/http-dashboard/app/scenes/UserProfile/components/UserProfile/components/AccountSettings/index.js
+++ server/http-dashboard/app/scenes/UserProfile/components/UserProfile/components/AccountSettings/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Section, Item} from '../../../Section';
-import {Button, Modal, message} from 'antd';
+import { Section, Item } from '../../../Section';
+import { Button, Modal, message } from 'antd';
 import Field from '../../../Field';
 
 import './styles.less';
-import {displayError} from "services/ErrorHandling";
+import { displayError } from "services/ErrorHandling";
 
 class MyAccount extends React.Component {
 
@@ -12,7 +12,7 @@ class MyAccount extends React.Component {
     Account: React.PropTypes.object,
     AccountFetch: React.PropTypes.func,
     onAccountNameUpdate: React.PropTypes.func,
-    onAccountSave : React.PropTypes.func,
+    onAccountSave: React.PropTypes.func,
     onAccountResetPassword: React.PropTypes.func,
   };
 
@@ -20,20 +20,23 @@ class MyAccount extends React.Component {
     super(props);
 
     this.handleNameSave = this.handleNameSave.bind(this);
-    this.resetPassword  = this.resetPassword.bind(this);
+    this.resetPassword = this.resetPassword.bind(this);
   }
 
   handleNameSave(name) {
     const hideUpdatingMessage = message.loading('Updating account name..', 0);
     this.props.onAccountNameUpdate(name);
     /** @todo track error */
-    this.props.onAccountSave(Object.assign({}, this.props.Account, {name: name})).then(() => {
+    this.props.onAccountSave(Object.assign({}, this.props.Account, { name: name })).then(() => {
       hideUpdatingMessage();
+    }).catch(error => {
+      hideUpdatingMessage();
+      displayError(error, message.error);
     });
   }
 
   resetPassword() {
-    this.props.onAccountResetPassword(Object.assign({}, {email: this.props.Account.email})).then(() => {
+    this.props.onAccountResetPassword(Object.assign({}, { email: this.props.Account.email })).then(() => {
       this.showResetPasswordSuccessMessage();
     }).catch((err) => {
       this.showResetPasswordErrorMessage(err);
@@ -61,7 +64,8 @@ class MyAccount extends React.Component {
       <div className="user-profile">
 
         <Item title="Name">
-          <Field value={this.props.Account.name} onChange={this.handleNameSave}/>
+          <Field value={this.props.Account.name}
+                 onChange={this.handleNameSave}/>
         </Item>
         <Item title="Email Address">
           {this.props.Account.email}
