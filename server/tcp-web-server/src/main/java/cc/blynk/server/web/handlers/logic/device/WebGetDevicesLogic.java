3e48acb9e24d57694d3d6432f270f0d64bbff676
Dmitriy Dumanskiy <doom369@gmail.com>
2018-11-24 12:22:09 +0200

    #1716 throw is organization is not found

--- server/tcp-web-server/src/main/java/cc/blynk/server/web/handlers/logic/device/WebGetDevicesLogic.java
+++ server/tcp-web-server/src/main/java/cc/blynk/server/web/handlers/logic/device/WebGetDevicesLogic.java
@@ -48,7 +48,7 @@ public final class WebGetDevicesLogic {
             return;
         }
 
-        Organization org = organizationDao.getOrgById(orgId);
+        Organization org = organizationDao.getOrgByIdOrThrow(orgId);
 
         String devicesJson;
         List<DeviceDTO> deviceDTOs;
