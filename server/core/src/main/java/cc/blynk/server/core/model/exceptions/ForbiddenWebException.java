aed6ef2e269fe578b7e88396a15065b54ba496cb
Dmitriy Dumanskiy <doom369@gmail.com>
2018-12-13 16:44:16 +0200

    WebException removed

--- server/core/src/main/java/cc/blynk/server/core/model/exceptions/ForbiddenWebException.java
+++ server/core/src/main/java/cc/blynk/server/core/model/exceptions/ForbiddenWebException.java
@@ -1,11 +1,13 @@
 package cc.blynk.server.core.model.exceptions;
 
+import cc.blynk.server.core.protocol.exceptions.JsonException;
+
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 19.06.17.
  */
-public class ForbiddenWebException extends WebException {
+public class ForbiddenWebException extends JsonException {
 
     public ForbiddenWebException(String message) {
         super(message);
--- server/core/src/main/java/cc/blynk/server/core/model/exceptions/OrgNotFoundException.java
+++ server/core/src/main/java/cc/blynk/server/core/model/exceptions/OrgNotFoundException.java
@@ -1,11 +1,13 @@
 package cc.blynk.server.core.model.exceptions;
 
+import cc.blynk.server.core.protocol.exceptions.JsonException;
+
 /**
  * The Blynk Project.
  * Created by Dmitriy Dumanskiy.
  * Created on 19.06.17.
  */
-public class OrgNotFoundException extends WebException {
+public class OrgNotFoundException extends JsonException {
 
     public OrgNotFoundException(String message) {
         super(message);
