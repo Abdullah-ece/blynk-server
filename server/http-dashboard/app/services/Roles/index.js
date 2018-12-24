ca5636ae5cb7a7cd3016ad60d1120009bd73de80
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2018-12-24 03:54:10 +0200

    #1936 - moved permissions and related data to the service       - added permissions verification

--- server/http-dashboard/app/services/Roles/index.js
+++ server/http-dashboard/app/services/Roles/index.js
@@ -89,8 +89,254 @@ export {
 
 export const isUserAbleToEdit = (userRole, fieldRole) => {
 
-  if(userRole === SUPER_ADMIN_ROLE_ID)
+  if (userRole === SUPER_ADMIN_ROLE_ID)
     return true;
 
   return (Array.isArray(fieldRole) ? fieldRole : [fieldRole]).indexOf(Number(userRole)) !== -1;
+};
+
+export const PERMISSIONS_INDEX = {
+  ORG_SWITCH: 0,
+  OWN_ORG_EDIT: 1,
+  OTA_VIEW: 2,
+  OTA_START: 3,
+  OTA_STOP: 4,
+
+  ORG_CREATE: 5,
+  ORG_VIEW: 6,
+  ORG_EDIT: 7,
+  ORG_DELETE: 8,
+
+  ORG_INVITE_USERS: 9,
+  ORG_VIEW_USERS: 10,
+  ORG_EDIT_USERS: 11,
+  ORG_DELETE_USERS: 12,
+
+  PRODUCT_CREATE: 13,
+  PRODUCT_VIEW: 14,
+  PRODUCT_EDIT: 15,
+  PRODUCT_DELETE: 16,
+
+  ROLE_CREATE: 17,
+  ROLE_VIEW: 18,
+  ROLE_EDIT: 19,
+  ROLE_DELETE: 20,
+
+  ORG_DEVICES_CREATE: 21,
+  ORG_DEVICES_VIEW: 22,
+  ORG_DEVICES_EDIT: 23,
+  ORG_DEVICES_DELETE: 24,
+
+  ORG_DEVICES_SHARE: 25,
+  OWN_DEVICES_CREATE: 26,
+  OWN_DEVICES_VIEW: 27,
+  OWN_DEVICES_EDIT: 28,
+  OWN_DEVICES_DELETE: 29,
+  OWN_DEVICES_SHARE: 30,
+
+  SET_AUTH_TOKEN: 31,
+};
+
+export const PERMISSIONS_TABLE = [
+  {
+    dependentPermsToActivate: [6],
+    dependentPermsToRemove: [],
+    key: 'ORG_SWITCH',
+    value: 'Switch to Sub-Organizations'
+  },
+  {
+    dependentPermsToActivate: [6],
+    dependentPermsToRemove: [],
+    key: 'OWN_ORG_EDIT',
+    value: 'Access Organization Settings'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'OTA_VIEW',
+    value: 'Get Access'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'OTA_START',
+    value: 'Initiate FOTA'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'OTA_STOP',
+    value: 'Stop/Pause FOTA'
+  },
+  {
+    dependentPermsToActivate: [6],
+    dependentPermsToRemove: [],
+    key: 'ORG_CREATE',
+    value: 'Create'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [5, 7, 8, 0, 1],
+    key: 'ORG_VIEW',
+    value: 'View'
+  },
+  {
+    dependentPermsToActivate: [6],
+    dependentPermsToRemove: [],
+    key: 'ORG_EDIT',
+    value: 'Edit'
+  },
+  {
+    dependentPermsToActivate: [6],
+    dependentPermsToRemove: [],
+    key: 'ORG_DELETE',
+    value: 'Delete'
+  },
+  {
+    dependentPermsToActivate: [10],
+    dependentPermsToRemove: [],
+    key: 'ORG_INVITE_USERS',
+    value: 'Invite new users'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [9, 11, 12],
+    key: 'ORG_VIEW_USERS',
+    value: 'View'
+  },
+  {
+    dependentPermsToActivate: [10],
+    dependentPermsToRemove: [],
+    key: 'ORG_EDIT_USERS',
+    value: 'Edit'
+  },
+  {
+    dependentPermsToActivate: [10],
+    dependentPermsToRemove: [],
+    key: 'ORG_DELETE_USERS',
+    value: 'Delete'
+  },
+  {
+    dependentPermsToActivate: [14],
+    dependentPermsToRemove: [],
+    key: 'PRODUCT_CREATE',
+    value: 'Add new'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [13, 15, 16],
+    key: 'PRODUCT_VIEW',
+    value: 'View'
+  },
+  {
+    dependentPermsToActivate: [14],
+    dependentPermsToRemove: [],
+    key: 'PRODUCT_EDIT',
+    value: 'Edit'
+  },
+  {
+    dependentPermsToActivate: [14],
+    dependentPermsToRemove: [],
+    key: 'PRODUCT_DELETE',
+    value: 'Delete'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ROLE_CREATE',
+    value: 'Create new roles'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ROLE_VIEW',
+    value: 'View roles and permissions'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ROLE_EDIT',
+    value: 'Edit roles'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ROLE_DELETE',
+    value: 'Delete roles'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ORG_DEVICES_CREATE',
+    value: 'Add new devices'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [23, 24, 31],
+    key: 'ORG_DEVICES_VIEW',
+    value: 'View'
+  },
+  {
+    dependentPermsToActivate: [22],
+    dependentPermsToRemove: [31],
+    key: 'ORG_DEVICES_EDIT',
+    value: 'Edit'
+  },
+  {
+    dependentPermsToActivate: [22],
+    dependentPermsToRemove: [],
+    key: 'ORG_DEVICES_DELETE',
+    value: 'Delete'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'ORG_DEVICES_SHARE',
+    value: 'Share access'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'OWN_DEVICES_CREATE',
+    value: 'Add new devices'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [28, 29, 31],
+    key: 'OWN_DEVICES_VIEW',
+    value: 'View'
+  },
+  {
+    dependentPermsToActivate: [27],
+    dependentPermsToRemove: [31],
+    key: 'OWN_DEVICES_EDIT',
+    value: 'Edit'
+  },
+  {
+    dependentPermsToActivate: [27],
+    dependentPermsToRemove: [],
+    key: 'OWN_DEVICES_DELETE',
+    value: 'Delete'
+  },
+  {
+    dependentPermsToActivate: [],
+    dependentPermsToRemove: [],
+    key: 'OWN_DEVICES_SHARE',
+    value: 'Share access'
+  },
+  {
+    dependentPermsToActivate: [27, 28],
+    dependentPermsToRemove: [],
+    key: 'SET_AUTH_TOKEN',
+    value: 'Enable Auth Token Edit'
+  },
+];
+
+export const VerifyPermission = (userPermissions, permissionToCheck) => {
+  console.log(permissionToCheck)
+  const newPermissions = Math.pow(2, permissionToCheck);
+  console.log(newPermissions, userPermissions & newPermissions)
+  const result = (userPermissions & newPermissions) == newPermissions;
+  console.log(result);
+  return result;
 };
