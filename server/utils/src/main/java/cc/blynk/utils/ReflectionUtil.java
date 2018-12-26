b1d6b4719316c35162d7e908b86796e2f8ba103b
Dmitriy Dumanskiy <doom369@gmail.com>
2018-12-26 14:48:50 +0200

    cleanup

--- server/utils/src/main/java/cc/blynk/utils/ReflectionUtil.java
+++ server/utils/src/main/java/cc/blynk/utils/ReflectionUtil.java
@@ -28,6 +28,33 @@ public final class ReflectionUtil {
         return valuesName;
     }
 
+    public static Map<Short, String> generateMapOfValueNameShort(Class<?> clazz) {
+        var valuesName = new HashMap<Short, String>();
+        try {
+            for (var field : clazz.getFields()) {
+                if (field.getType() == short.class) {
+                    valuesName.put((Short) field.get(short.class), formatCommandName(field.getName()));
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return valuesName;
+    }
+
+    private static String formatCommandName(String fieldName) {
+        String[] split = fieldName.split("_");
+        StringBuilder sb = new StringBuilder();
+        for (String s : split) {
+            sb.append(capitilizeFirstLetter(s));
+        }
+        return sb.toString();
+    }
+
+    private static String capitilizeFirstLetter(String name) {
+        return name.substring(0, 1) + name.substring(1).toLowerCase();
+    }
+
     public static Object castTo(Class type, String value) {
         if (type == byte.class || type == Byte.class) {
             return Byte.valueOf(value);
