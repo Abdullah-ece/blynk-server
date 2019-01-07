5958ff7e521ef0cf728f71c83b243f23a46afbf7
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2019-01-08 00:47:37 +0200

    #1985 - added layout

--- server/http-dashboard/app/scenes/RuleEngine/Index/index.js
+++ server/http-dashboard/app/scenes/RuleEngine/Index/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { MainList, MainLayout } from 'components';
+import { MainLayout } from 'components';
+import { Button } from 'antd';
+import '../styless.less';
 
 class Index extends React.Component {
 
   render() {
     return (
-      <MainLayout.Content className="organizations-empty">
-        <MainList>
-          <MainList.Empty title="Business analytics at your fingertips"/>
-        </MainList>
-      </MainLayout.Content>
+      <MainLayout>
+        <MainLayout.Header title="Rules Engine"
+                           options={(
+                             <div>
+                               <Button type="primary">Update Rules</Button>
+                             </div>
+
+                           )}
+        />
+        <MainLayout.Content
+          className="layout-content-rules-engine-text-area product-edit-content">
+          <textarea className="rules-engine-text-area"></textarea>
+        </MainLayout.Content>
+      </MainLayout>
     );
   }
-
 }
 
 export default Index;
