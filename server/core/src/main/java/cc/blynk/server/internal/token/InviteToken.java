4c4398988637b0bf696e6a603fabaf084c89bf7c
Dmitriy Dumanskiy <doom369@gmail.com>
2019-01-07 18:46:58 +0200

    #1986 increase invite expiration to 60 days

--- server/core/src/main/java/cc/blynk/server/internal/token/InviteToken.java
+++ server/core/src/main/java/cc/blynk/server/internal/token/InviteToken.java
@@ -1,6 +1,7 @@
 package cc.blynk.server.internal.token;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The Blynk Project.
@@ -9,12 +10,14 @@ import java.io.Serializable;
  */
 public final class InviteToken extends BaseToken implements Serializable {
 
+    private static final long INVITE_EXPIRE_TIME = TimeUnit.DAYS.toMillis(60);
+
     public final int orgId;
     public final String appName;
     public final String email;
 
     public InviteToken(String email, int orgId, String appName) {
-        super(DEFAULT_EXPIRE_TIME);
+        super(INVITE_EXPIRE_TIME);
         this.email = email;
         this.orgId = orgId;
         this.appName = appName;
