9d11122385e7f13446e4b3162407d01b84af5404
Nikita Piashyntsev <piashyntsev.ns@gmail.com>
2019-02-01 16:51:37 +0200

    Added one more test for #2072

--- integration-tests/src/test/java/cc/blynk/integration/web/reporting/ReportingStatsTest.java
+++ integration-tests/src/test/java/cc/blynk/integration/web/reporting/ReportingStatsTest.java
@@ -1,7 +1,10 @@
 package cc.blynk.integration.web.reporting;
 
 import cc.blynk.integration.SingleServerInstancePerTestWithDB;
+import cc.blynk.integration.model.tcp.ClientPair;
+import cc.blynk.server.core.model.device.Device;
 import cc.blynk.server.core.stats.model.Stat;
+import cc.blynk.server.workers.StatsWorker;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +15,12 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Map;
 
+import static cc.blynk.integration.TestUtil.hardware;
+import static cc.blynk.server.core.protocol.enums.Command.HARDWARE;
+import static cc.blynk.server.core.protocol.enums.Command.MOBILE_GET_DEVICE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * The Blynk Project.
@@ -69,13 +77,50 @@ public class ReportingStatsTest extends SingleServerInstancePerTestWithDB {
 
         try (Connection connection = holder.reportingDBManager.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet rs = statement.executeQuery("select * from reporting_command_stat_minute")) {
+             ResultSet rs = statement.executeQuery("select region, command_code, counter from reporting_command_stat_minute")) {
             while (rs.next()) {
                 assertEquals(UA, rs.getString("region"));
-                rs.getTimestamp("ts");
 
                 short commandCode = rs.getShort("command_code");
                 assertEquals(stat.allCommands.stats.get(commandCode), (Integer) rs.getInt("counter"));
+            }
+
+            connection.commit();
+        }
+    }
+
+    @Test
+    public void testStatsWorkerHavingHardwareRecord() throws Exception {
+        ClientPair clientPair = initAppAndHardPair();
+        var statsWorker = new StatsWorker(holder);
+
+        Device device = new Device();
+        device.name = "My New Device";
+
+        clientPair.appClient.createDevice(device);
+        device = clientPair.appClient.parseDevice(1);
+        assertNotNull(device);
+
+        clientPair.appClient.getDevice(device.id);
+        device = clientPair.appClient.parseDevice(2);
+
+        clientPair.hardwareClient.send("hardware vw 1 1");
+        clientPair.appClient.verifyResult(hardware(1, device.id - 1 + " vw 1 1"));
+
+        statsWorker.run();
+
+        try (Connection connection = holder.reportingDBManager.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet rs = statement.executeQuery("select region, command_code, counter from reporting_command_stat_minute")) {
+            while (rs.next()) {
+                assertEquals("test-region", rs.getString("region"));
+
+                short commandCode = rs.getShort("command_code");
+                int counter   = rs.getInt("counter");
+                if (commandCode == HARDWARE ||
+                        commandCode == MOBILE_GET_DEVICE) {
+                    assertTrue(counter > 0);
+                }
             }
 
             connection.commit();
