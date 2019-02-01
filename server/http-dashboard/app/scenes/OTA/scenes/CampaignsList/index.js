55f0440ebb0076cbe8e6477ea71e968ecb91d7b3
Vlad Martynenko <vladimir.martynenko.work@gmail.com>
2019-02-01 06:10:11 +0200

    - OTA list basic layout

--- server/http-dashboard/app/scenes/OTA/scenes/CampaignsList/index.js
+++ server/http-dashboard/app/scenes/OTA/scenes/CampaignsList/index.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { Button } from "antd";
+import { Table } from "antd";
 
 class CampaignsList extends Component {
+  static contextTypes = {
+    router: React.PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
   render() {
     return (
-      <div></div>
+      <MainLayout>
+        <MainLayout.Header
+          title={'All Shippings'}
+          options={(
+            <div>
+              <Button type="primary"
+                      onClick={() => this.context.router.push('/ota/create')}>
+                New Shipping
+              </Button>
+            </div>
+          )}/>
+        <MainLayout.Content className="organizations-create-content">
+          <div>
+            <a href="#">All Shippings ({this.props.shippings.length})</a>
+          </div>
+          <div>
+            <Table/>
+          </div>
+        </MainLayout.Content>
+      </MainLayout>
     );
   }
 }
